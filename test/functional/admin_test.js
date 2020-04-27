fc217d4ae36f960fea3de98596463f2297d4152a
shaung <s.ganley@kainos.com>
2020-04-27 15:27:41 +0100

    adding divorce roles required by functional tests

--- test/functional/admin_test.js
+++ test/functional/admin_test.js
@@ -7,12 +7,16 @@ Scenario('add all the roles', I => {
   I.createRole('caseworker-divorce-financialremedy-courtadmin');
   I.createRole('caseworker-divorce-financialremedy-solicitor');
   I.createRole('caseworker-divorce-financialremedy-judiciary');
+  I.createRole('caseworker-divorce-systemupdate');
+  I.createRole('caseworker-divorce-bulkscan');
   I.click('Manage User Roles');
   I.see('citizen');
   I.see('caseworker');
   I.see('caseworker-divorce-financialremedy-courtadmin');
   I.see('caseworker-divorce-financialremedy-solicitor');
   I.see('caseworker-divorce-financialremedy-judiciary');
+  I.see('caseworker-divorce-systemupdate');
+  I.see('caseworker-divorce-bulkscan');
 }).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
 
 Scenario('upload Consented Config file', I => {
