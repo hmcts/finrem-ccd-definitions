7c9afc14ae2a030874f69d70184d77cbb78eba2d
shaunganley <s.ganley@kainos.com>
2020-04-29 17:33:56 +0100

    Bsp405 ccd env (#63)
    * BSP-405: Fix FR CCD build pipeline
    
    * update README
    
    * Trying changes that could result in successfully login into case management service
    
    * Adding default password to documentation
    
    Co-authored-by: J. Victor Ferreira <victor.ferreira@hmcts.net>

--- test/functional/admin_test.js
+++ test/functional/admin_test.js
@@ -4,33 +4,31 @@ Scenario('add all the roles', I => {
   I.loginToAdminConsole();
   I.createRole('citizen');
   I.createRole('caseworker');
-  I.createRole('caseworker-divorce-courtadmin');
-  I.createRole('caseworker-divorce-solicitor');
-  I.createRole('caseworker-divorce-courtadmin_beta');
+  I.createRole('caseworker-divorce-financialremedy-courtadmin');
+  I.createRole('caseworker-divorce-financialremedy-solicitor');
+  I.createRole('caseworker-divorce-financialremedy-judiciary');
   I.createRole('caseworker-divorce-systemupdate');
   I.createRole('caseworker-divorce-bulkscan');
-  I.createRole('caseworker-divorce-courtadmin-la');
-  I.createRole('caseworker-divorce-superuser');
+  I.createRole('caseworker-divorce-financialremedy');
   I.click('Manage User Roles');
   I.see('citizen');
   I.see('caseworker');
-  I.see('caseworker-divorce-courtadmin');
-  I.see('caseworker-divorce-solicitor');
-  I.see('caseworker-divorce-courtadmin_beta');
+  I.see('caseworker-divorce-financialremedy-courtadmin');
+  I.see('caseworker-divorce-financialremedy-solicitor');
+  I.see('caseworker-divorce-financialremedy-judiciary');
   I.see('caseworker-divorce-systemupdate');
   I.see('caseworker-divorce-bulkscan');
-  I.see('caseworker-divorce-courtadmin-la');
-  I.see('caseworker-divorce-superuser');
+  I.see('caseworker-divorce-financialremedy');
 }).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
 
 Scenario('upload Consented Config file', I => {
   I.loginToAdminConsole();
   I.uploadConfig('../../definitions/consented/xlsx/ccd-config-aat-consented.xlsx');
-  I.see('Consented Case Definition data successfully imported');
+  I.see('Case Definition data successfully imported');
 }).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
 
 Scenario('upload Contested Config file', I => {
   I.loginToAdminConsole();
   I.uploadConfig('../../definitions/contested/xlsx/ccd-config-aat-contested.xlsx');
-  I.see('Contested Case Definition data successfully imported');
+  I.see('Case Definition data successfully imported');
 }).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
