b2b1201875c9e4117e93a41ecbcf1c07c4febda3
vivek reddy <vivek.reddy@hmcts.net>
2020-08-03 12:38:13 +0100

    FR-1769-add consented, contested test creation (#169)

---
+++ test/functional/contested_test.js
@@ -0,0 +1,18 @@
+const { createCaseInCcd, updateCaseInCcd } = require('../helpers/utils');
+
+const ccdWebUrl = process.env.CCD_WEB_URL;
+const solicitorUserName = process.env.USERNAME_SOLICITOR;
+const solicitorPassword = process.env.PASSWORD_SOLICITOR;
+const caseWorkerUserName = process.env.USERNAME_CASEWORKER;
+const caseWorkerPassword = process.env.PASSWORD_CASEWORKER;
+
+Feature('create Contested case ');
+
+Scenario('Contested Case Creation', async I => {
+  const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-contested-basic-data.json', 'FinancialRemedyContested');
+  /* eslint-disable */
+  const caseSubmission = await updateCaseInCcd(solicitorUserName, solicitorPassword, caseId, 'FinancialRemedyContested', 'FR_applicationPaymentSubmission', './test/data/ccd-hwf-contested-payment.json');
+  const hwfPaymentAccepted = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyContested', 'FR_HWFDecisionMade', './test/data/ccd-contested-basic-data.json');
+  /* eslint-enable */
+  I.amOnPage(ccdWebUrl + caseId);
+});
---
+++ test/helpers/utils.js
@@ -0,0 +1,199 @@
+const { Logger } = require('@hmcts/nodejs-logging');
+const requestModule = require('request-promise-native');
+
+const request = requestModule.defaults({ proxy: 'http://proxyout.reform.hmcts.net:8080' });
+const fs = require('fs');
+
+const logger = Logger.getLogger('helpers/utils.js');
+
+const env = process.env.RUNNING_ENV || 'aat';
+
+async function getUserToken(username, password) {
+  logger.info('Getting User Token');
+  const redirectUri = `https://div-pfe-${env}.service.core-compute-${env}.internal/authenticated`;
+  const idamClientSecret = process.env.IDAM_CLIENT_SECRET;
+  const idamBaseUrl = 'https://idam-api.aat.platform.hmcts.net';
+
+  const idamCodePath = `/oauth2/authorize?response_type=code&client_id=divorce&redirect_uri=${redirectUri}`;
+
+  const codeResponse = await request.post({
+    uri: idamBaseUrl + idamCodePath,
+    headers: {
+      Authorization: `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }).catch(error => {
+    console.log(error);
+  });
+
+  const code = JSON.parse(codeResponse).code;
+
+  const idamAuthPath = `/oauth2/token?grant_type=authorization_code&client_id=divorce&client_secret=${idamClientSecret}&redirect_uri=${redirectUri}&code=${code}`;
+  const authTokenResponse = await request.post({
+    uri: idamBaseUrl + idamAuthPath,
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+  });
+
+  logger.debug(JSON.parse(authTokenResponse).access_token);
+
+  return JSON.parse(authTokenResponse).access_token;
+}
+
+async function getUserId(authToken) {
+  logger.info('Getting User Id');
+
+  const idamBaseUrl = 'https://idam-api.aat.platform.hmcts.net';
+
+  const idamDetailsPath = '/details';
+  const userDetails = await request.get({
+    uri: idamBaseUrl + idamDetailsPath,
+    headers: { Authorization: `Bearer ${authToken}` }
+  });
+
+  logger.debug(JSON.parse(userDetails).id);
+
+  return JSON.parse(userDetails).id;
+}
+
+async function getServiceToken() {
+  logger.info('Getting Service Token');
+
+  const serviceSecret = process.env.CCD_SUBMIT_S2S_SECRET;
+
+  const s2sBaseUrl = `http://rpe-service-auth-provider-${env}.service.core-compute-${env}.internal`;
+  const s2sAuthPath = '/lease';
+  // eslint-disable-next-line global-require
+  const oneTimePassword = require('otp')({ secret: serviceSecret }).totp();
+
+  const serviceToken = await request({
+    method: 'POST',
+    uri: s2sBaseUrl + s2sAuthPath,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      microservice: 'divorce_ccd_submission',
+      oneTimePassword
+    })
+  });
+
+  logger.debug(serviceToken);
+
+  return serviceToken;
+}
+
+async function createCaseInCcd(userName, password, dataLocation, caseType) {
+  const authToken = await getUserToken(userName, password);
+
+  const userId = await getUserId(authToken);
+
+  const serviceToken = await getServiceToken();
+
+  logger.info('Creating Case');
+
+  const ccdApiUrl = process.env.CCD_DATA_API_URL;
+  const frCaseType = caseType;
+  const ccdStartCasePath = `/caseworkers/${userId}/jurisdictions/DIVORCE/case-types/${frCaseType}/event-triggers/FR_solicitorCreate/token`;
+  const ccdSaveCasePath = `/caseworkers/${userId}/jurisdictions/DIVORCE/case-types/${frCaseType}/cases`;
+
+  const startCaseOptions = {
+    method: 'GET',
+    uri: ccdApiUrl + ccdStartCasePath,
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+      ServiceAuthorization: `Bearer ${serviceToken}`,
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const startCaseResponse = await request(startCaseOptions);
+  console.log(startCaseResponse);
+  const eventToken = JSON.parse(startCaseResponse).token;
+  /* eslint id-blacklist: ["error", "undefined"] */
+  const data = fs.readFileSync(dataLocation);
+  const saveBody = {
+    data: JSON.parse(data),
+    event: {
+      id: 'FR_solicitorCreate',
+      summary: 'Creating Basic Case',
+      description: 'For CCD E2E Test'
+    },
+    event_token: eventToken
+  };
+
+  const saveCaseOptions = {
+    method: 'POST',
+    uri: ccdApiUrl + ccdSaveCasePath,
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+      ServiceAuthorization: `Bearer ${serviceToken}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(saveBody)
+  };
+
+  const saveCaseResponse = await request(saveCaseOptions).catch(error => {
+    console.log(error);
+  });
+
+  const caseId = JSON.parse(saveCaseResponse).id;
+
+  logger.info('Created case with id %s', caseId);
+
+  return caseId;
+}
+
+async function updateCaseInCcd(userName, password, caseId, caseType, eventId, dataLocation) {
+  const authToken = await getUserToken(userName, password);
+
+  const userId = await getUserId(authToken);
+
+  const serviceToken = await getServiceToken();
+
+  logger.info('Updating case with id %s and event %s', caseId, eventId);
+
+  const ccdApiUrl = process.env.CCD_DATA_API_URL;
+  const frCaseType = caseType;
+  const ccdStartEventPath = `/caseworkers/${userId}/jurisdictions/DIVORCE/case-types/${frCaseType}/cases/${caseId}/event-triggers/${eventId}/token`;
+  const ccdSaveEventPath = `/caseworkers/${userId}/jurisdictions/DIVORCE/case-types/${frCaseType}/cases/${caseId}/events`;
+
+  const startEventOptions = {
+    method: 'GET',
+    uri: ccdApiUrl + ccdStartEventPath,
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+      ServiceAuthorization: `Bearer ${serviceToken}`,
+      'Content-Type': 'application/json'
+    }
+  };
+
+  const startEventResponse = await request(startEventOptions);
+
+  const eventToken = JSON.parse(startEventResponse).token;
+
+  const data = fs.readFileSync(dataLocation);
+  const saveBody = {
+    data: JSON.parse(data),
+    event: {
+      id: eventId,
+      summary: 'Updating Case',
+      description: 'For CCD E2E Test'
+    },
+    event_token: eventToken
+  };
+
+  const saveEventOptions = {
+    method: 'POST',
+    uri: ccdApiUrl + ccdSaveEventPath,
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+      ServiceAuthorization: `Bearer ${serviceToken}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(saveBody)
+  };
+
+  const saveEventResponse = await request(saveEventOptions);
+
+  return saveEventResponse;
+}
+
+module.exports = { createCaseInCcd, updateCaseInCcd };
