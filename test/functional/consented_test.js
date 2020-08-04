e2730819421a635cde70ec528a25919ea4d77422
vivek reddy <vivek.reddy@hmcts.net>
2020-08-04 14:01:52 +0100

    added tests for allocating cases to judge (#173)

--- test/functional/consented_test.js
+++ test/functional/consented_test.js
@@ -8,11 +8,21 @@ const caseWorkerPassword = process.env.PASSWORD_CASEWORKER;
 
 Feature('create Consented case ');
 
-Scenario('Consent Case Creation', async I => {
+Scenario('Consent Case Creation For Caseworker', async I => {
   const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-consented-basic-data.json', 'FinancialRemedyMVP2');
   /* eslint-disable */
   const caseSubmission = await updateCaseInCcd(solicitorUserName, solicitorPassword, caseId, 'FinancialRemedyMVP2', 'FR_applicationPaymentSubmission', './test/data/ccd-hwf-consented-payment.json');
   const hwfPaymentAccepted = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyMVP2', 'FR_HWFDecisionMade', './test/data/ccd-consented-basic-data.json');
+  /* eslint-enable */
+  I.amOnPage(ccdWebUrl + caseId);
+});
+
+Scenario('Consent Case Creation For Judge', async I => {
+  const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-consented-basic-data.json', 'FinancialRemedyMVP2');
+  /* eslint-disable */
+  const caseSubmission = await updateCaseInCcd(solicitorUserName, solicitorPassword, caseId, 'FinancialRemedyMVP2', 'FR_applicationPaymentSubmission', './test/data/ccd-hwf-consented-payment.json');
+  const hwfPaymentAccepted = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyMVP2', 'FR_HWFDecisionMade', './test/data/ccd-consented-basic-data.json');
+  const issueApplication = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyMVP2', 'FR_issueApplication', './test/data/ccd-consented-case-worker-issue-data.json');
   /* eslint-enable */
   I.amOnPage(ccdWebUrl + caseId);
 });
