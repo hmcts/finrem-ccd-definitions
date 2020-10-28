e33ec446145a140e4b93cd413344a2e5ac220acb
vianvi <30834324+vianvi@users.noreply.github.com>
2020-10-28 10:34:25 +0000

    typo error on url

--- test/functional/pages/create-case/create-case.js
+++ test/functional/pages/create-case/create-case.js
@@ -2,13 +2,10 @@
 
 function createCase(type, event) {
   const I = this;
-  // I.wait(15);
-  I.waitForElement('a[href="/create/case"]', '60');
-  I.click('Create new case');
+  I.waitForElement('a[href="/cases/case-filter"]', '60');
+  I.click('Create case');
   I.waitForPage('h1', 'Create Case');
-
   I.waitForElement('select[id="cc-jurisdiction"]>option:nth-of-type(2)', '60');
-
   I.selectOption('select[id="cc-jurisdiction"]', 'Family Divorce');
   I.wait('1');
   I.selectOption('select[id="cc-case-type"]', type);
