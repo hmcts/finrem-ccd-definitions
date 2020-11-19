59d02d5bf05ee5f56dcaa0d4e81cca6da378e9d9
vivek reddy <vivek.reddy@hmcts.net>
2020-11-19 16:17:40 +0000

    add search organisation on solicitor journey tests (#325)

--- test/functional/pages/respondent-details/respondent-details.js
+++ test/functional/pages/respondent-details/respondent-details.js
@@ -4,8 +4,9 @@ function respondentSolicitorDetailsDetails(I) {
   I.fillField('input[id="rSolicitorName"]', 'ResSol');
   I.fillField('input[id="rSolicitorFirm"]', 'MahFirm');
   I.fillField('input[id="rSolicitorReference"]', 'MahSol73');
-  I.fillField('input[id="RespondentOrganisationPolicy_Organisation_OrganisationID"]', 'repondFirmxyz');
-  I.fillField('input[id="RespondentOrganisationPolicy_OrgPolicyCaseAssignedRole"]', 'respondent solicitor');
+  I.fillField('input[id="search-org-text"]', 'financialremedyrespondentorg');
+  I.click('Select');
+  I.waitForText('SW1H 9AJ');
   I.fillField('input[id="RespondentOrganisationPolicy_OrgPolicyReference"]', 'res-sox');
   I.fillField('Enter a UK postcode', 'TW3 1SS');
   I.click('Find address');
