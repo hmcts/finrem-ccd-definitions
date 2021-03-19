331ebc1d3c3cc8077f2aeaab310751ebf2d3d601
vianvi <30834324+vianvi@users.noreply.github.com>
2021-03-19 17:09:44 +0000

    test commit

--- test/accessibility/runner.js
+++ test/accessibility/runner.js
@@ -26,7 +26,7 @@ function updateResultObject(url, pageTitle, screenshotReportRef, accessibilityEr
   }
 
   resultObj.tests.push({
-    name: `${urlArr[urlArr.length - '2']}/${urlArr[urlArr.length - '1']}`,
+    name: `${urlArr[urlArr.length - 2]}/${urlArr[urlArr.length - 1]}`,
     pageUrl: url,
     documentTitle: pageTitle,
     status: isPageAccessible,
