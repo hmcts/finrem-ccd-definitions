6ef5b6bcfd8cb0fadaec630716d0b3868d3f40d8
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2021-10-22 12:29:55 +0100

    comment out admin test

--- test/functional/admin_test.js
+++ test/functional/admin_test.js
@@ -1,6 +1,6 @@
-Feature('Admin Web');
+Feature('Admin Web -- commented out these test cases as I dont think we should run these tests');
 
-Scenario('add all the roles @pipeline', I => {
+xScenario('add all the roles @pipeline', I => {
   I.loginToAdminConsole();
   I.createRole('citizen');
   I.createRole('caseworker');
@@ -23,13 +23,13 @@ Scenario('add all the roles @pipeline', I => {
   I.see('caseworker-caa');
 }).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
 
-Scenario('upload Consented Config file @pipeline', I => {
+xScenario('upload Consented Config file @pipeline', I => {
   I.loginToAdminConsole();
   I.uploadConfig(`../../definitions/consented/xlsx/${process.env.CCD_CONSENTED_FILE_NAME}`);
   I.see('Case Definition data successfully imported');
 }).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
 
-Scenario('upload Contested Config file @pipeline', I => {
+xScenario('upload Contested Config file @pipeline', I => {
   I.loginToAdminConsole();
   I.uploadConfig(`../../definitions/contested/xlsx/${process.env.CCD_CONTESTED_FILE_NAME}`);
   I.see('Case Definition data successfully imported');
