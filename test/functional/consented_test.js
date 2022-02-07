11eb4370d2e77e3227fb7e1ecb3c87c0bcdea9d3
Tony <anatolysan2@gmail.com>
2022-02-07 14:55:21 +0000

    Remove tests for now

--- test/functional/consented_test.js
+++ test/functional/consented_test.js
@@ -59,23 +59,6 @@ Scenario('Consent Case Creation For Judge @nightly @pipeline', async I => {
   }
 });
 
-Scenario('Consent Case approve and send order @nightly @pipeline @crossBrowser', async I => {
-    const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-consented-basic-data.json', 'FinancialRemedyMVP2', 'FR_solicitorCreate');
-    const caseSubmission = await updateCaseInCcd(solicitorUserName, solicitorPassword, caseId, 'FinancialRemedyMVP2', 'FR_applicationPaymentSubmission', './test/data/ccd-hwf-consented-payment.json');
-    const hwfPaymentAccepted = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyMVP2', 'FR_HWFDecisionMade', './test/data/ccd-consented-basic-data.json');
-    const issueApplication = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyMVP2', 'FR_issueApplication', './test/data/ccd-consented-case-worker-issue-data.json');
-    const approveOrder = await updateCaseInCcd(judgeUserName, judgePassword, caseId, 'FinancialRemedyMVP2', 'FR_approveApplication', './test/data/ccd-consented-judge-approve-data.json');
-    const sendOrder = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyMVP2', 'FR_sendOrderForApproved', './test/data/ccd-caseworker-send-order.json');
-
-  /* eslint-enable */
-    if (nightlyTest === 'true') {
-      I.signInIdam(caseWorkerUserName, caseWorkerPassword);
-      I.amOnPage(`${ccdWebUrl}/v2/case/${caseId}`);
-      // eslint-disable-next-line max-len
-      I.verifyConsentedTabData(verifyTabText.caseType, verifyTabText.historyTab.sendOrderEvent, verifyTabText.historyTab.approveSendOrderEndState);
-      I.approvedOrderTab(verifyTabText.caseType, verifyTabText.approvedOrderTab.tabName);
-  }
-});
 /* eslint-disable require-await */
 Scenario('Consent Case Creation by Solicitor @nightly @crossBrowser', async I => {
   if (nightlyTest === 'true') {
