38e0af6ae073dcc82864f2be3c484803ab50919e
prabhamuthu15 <praba.muthu@justice.gov.uk>
2022-04-20 15:43:31 +0100

    DFR-997 UpdateE2E Test For CP

--- test/functional/pages/check-your-answers/check-your-answers.js
+++ test/functional/pages/check-your-answers/check-your-answers.js
@@ -8,7 +8,7 @@ async function checkYourAnswers() {
     await I.runAccessibilityTest();
   }
   I.see('SOLICITOR DETAILS');
-  I.see('Divorce / Dissolution Details');
+  I.see('DIVORCE / DISSOLUTION DETAILS');
   I.see('APPLICANT DETAILS');
   I.see('RESPONDENT DETAILS');
   I.wait('2');
