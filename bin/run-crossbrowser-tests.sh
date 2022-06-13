f7b31b0830417c4a0e58f423e3db601ddcd828f4
prabhamuthu15 <praba.muthu@justice.gov.uk>
2022-06-13 19:08:59 +0100

    crossbrowser

--- bin/run-crossbrowser-tests.sh
+++ bin/run-crossbrowser-tests.sh
@@ -8,8 +8,8 @@ export E2E_ADD_WAIT_CROSSBROWSER=true
 
 EXIT_STATUS=0
 BROWSER_GROUP=chromeWindows yarn test-crossbrowser-e2e || EXIT_STATUS=$?
-#BROWSER_GROUP=firefoxWindows yarn test-crossbrowser-e2e || EXIT_STATUS=$?
-#BROWSER_GROUP=firefoxMac yarn test-crossbrowser-e2e || EXIT_STATUS=$?
+BROWSER_GROUP=firefoxWindows yarn test-crossbrowser-e2e || EXIT_STATUS=$?
+BROWSER_GROUP=firefoxMac yarn test-crossbrowser-e2e || EXIT_STATUS=$?
 
 # To be fixed in @DFR-584
 #BROWSER_GROUP=microsoft yarn test-crossbrowser-e2e || EXIT_STATUS=$?
