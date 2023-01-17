a5615f96c8847671a6a1389c3c404e3b6701d1a8
sunilrochani <sunil.rochani@hotmail.com>
2023-01-17 18:04:24 +0000

    DFR-1078: Tech Debt - Automate Finrem CCD definitions upload via Jenkins Pipeline

--- src/test/java/finrem/HighLevelDataSetupApp.java
+++ src/test/java/finrem/HighLevelDataSetupApp.java
@@ -1,6 +1,5 @@
 package finrem;
 
-import uk.gov.hmcts.befta.TestAutomationConfig;
 import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
 
@@ -8,6 +7,10 @@ import java.util.*;
 
 public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     private static final String definitionsPath = "ccd_definition";
+    private static final List<CcdEnvironment> SKIPPED_ENVS = Arrays.asList(
+            CcdEnvironment.DEMO,
+            CcdEnvironment.AAT,
+            CcdEnvironment.PROD);
 
     public HighLevelDataSetupApp(CcdEnvironment dataSetupEnvironment) {
         super(dataSetupEnvironment, definitionsPath);
@@ -25,9 +28,12 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
     @Override
     protected void doLoadTestData() {
         List<String> definitionFileResources = getAllDefinitionFilesToLoadAt(definitionsPath);
-        System.out.println(definitionFileResources
-                + " definition file is skipped on " +
-                 getDataSetupEnvironment());
-        //importDefinitions();
+        CcdEnvironment currentEnv = (CcdEnvironment) getDataSetupEnvironment();
+        if (currentEnv != null && !SKIPPED_ENVS.contains(currentEnv)) {
+            importDefinitions();
+        } else {
+            definitionFileResources.forEach(file ->
+                    System.out.println("definition file \"" + file + "\" is skipped on " + currentEnv));
+        }
     }
 }
