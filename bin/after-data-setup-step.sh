b7a7441cbc416d2a6a089ee3738a3a55faae160e
srochani <sunil.rochani@hotmail.com>
2023-01-19 09:38:53 +0000

    Revert "Dfr 1078 jenkins definitons auto (#1004)" (#1015)
    This reverts commit ebffc65650f92125d591e35bf2152d6e3201990a.

--- bin/after-data-setup-step.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mv $(pwd)/src/test/resources/ccd_definition/keepme ./
-rm -rf $(pwd)/src/test/resources/ccd_definition/* 2>/dev/null
-mv ./keepme $(pwd)/src/test/resources/ccd_definition/
