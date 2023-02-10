a546c663cac19ab6f130b83a6a06b6df1653e6e5
srochani <sunil.rochani@hotmail.com>
2023-02-10 09:17:18 +0000

    DFR-1776: FR - Upgrade to Yarn v3

--- bin/consented/generate-excel-aat-consented.sh
+++ bin/consented/generate-excel-aat-consented.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
 source default.env
 FR_ENV=aat CCD_DEF_COS_URL=$npm_package_config_aat_cosUrl CCD_DEF_CCD_URL=$npm_package_config_aat_ccdUrl CCD_DEF_AAC_URL=$npm_package_config_aat_aacUrl EXCLUDE='*-prod.json' yarn run generate-excel-consented
--- bin/consented/generate-excel-demo-consented.sh
+++ bin/consented/generate-excel-demo-consented.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-
 source default.env
 FR_ENV=demo CCD_DEF_COS_URL=$npm_package_config_demo_cosUrl CCD_DEF_CCD_URL=$npm_package_config_demo_ccdUrl CCD_DEF_AAC_URL=$npm_package_config_demo_aacUrl EXCLUDE='*-prod.json' yarn run generate-excel-consented
