5485c0f5ea7e41aa202547936ef08930a26f58f1
srochani <sunil.rochani@hotmail.com>
2023-02-13 11:38:26 +0000

    DFR-1776: FR - Upgrade to Yarn v3

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-git submodule update --init --recursive && pushd ccd-definition-processor/ && CI=true yarn install && popd
+git submodule update --init --recursive && pushd ccd-definition-processor/ && YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install && popd
