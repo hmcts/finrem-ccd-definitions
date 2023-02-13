d57708860baed498cfab05bcec9d3387b81e60f7
Nilamkanojiya <nilam_kanojiya@yahoo.com>
2023-02-13 10:46:31 +0000

    merge confidential documents tests (#1056)
    * Test trigger build pr
    
    * fixed contested journey
    
    * DFR-1746: nightly build fix
    
    * DFR-1746: nightly build fix
    
    * DFR-1746: nightly build fix
    
    * added urgent question
    
    * DFR-1746: nightly build fix
    
    * DFR-1746: nightly build fix
    
    * DFR-1746: nightly build fix
    
    * DFR-1746: nightly build fix
    
    * DFR-1746: nightly build fix
    
    * DFR-1746: nightly build fix
    
    * DFR-1746: nightly build fix
    
    * added caseworker matrimonial test
    
    * fixed functional test
    
    * fixed test in CI
    
    * fixed test in CI
    
    * fixed broken test
    
    * try test with multiple chunks
    
    * Fixed nightly crossbrowser tests
    
    * Fixed nightly crossbrowser tests
    
    * fix consented tests
    
    * revert script
    
    * add test to master
    
    * review comment
    
    * remove parallel test code
    
    * revert changes
    
    * add judge login
    
    * update test to pass them in CI
    
    * DFR-1199
    
    * DFR-1199-  test
    
    * fix divorce details function
    
    * fix CI
    
    * add timeout
    
    * add test to pipeline
    
    * add 2 tests to master
    
    * remove nightly test flag
    
    * comment out code
    
    * comment out code
    
    * fix preview url
    
    * add file
    
    * remove few tests from master pipeline
    
    * removecommented code
    
    ---------
    
    Co-authored-by: dawudgovuk <dawud.rahman@justice.gov.uk>
    Co-authored-by: rishisharma <rishi.sharma@hmcts.net>
    Co-authored-by: sunilrochani <sunil.rochani@hotmail.com>
    Co-authored-by: Ruban <ruban72@hotmail.com>

--- test/functional/steps_file.js
+++ test/functional/steps_file.js
@@ -26,7 +26,7 @@ const { divorceDetails, contestedDivorceDetails } = require('./pages/divorce-det
 const { waitForContinueButtonEnabled } = require('./pages/common/common');
 const { waitForPage } = require('./pages/common/common');
 const { createCase } = require('./pages/create-case/create-case');
-const { historyTab, applicantTab, respondentTab, divorceTab, natureOfApplicationTab, authorisationTab, caseDocumentsTab, paymentDetailsTab, judgeDetailsTab, adminNotesTab, contestedOrderTab, approvedOrderTab, gateKeepingAllocationsTab, schedulingAndListingTab, consentOrderProcessTab, verifyConsentedTabData, verifyContestedTabData, verifyContestedPaperTabData } = require('./pages/tab-data-verification/tab-data-verification');
+const { historyTab, applicantTab, respondentTab, divorceTab, natureOfApplicationTab, authorisationTab, caseDocumentsTab, paymentDetailsTab, judgeDetailsTab, adminNotesTab, contestedOrderTab, approvedOrderTab, gateKeepingAllocationsTab, schedulingAndListingTab, consentOrderProcessTab, verifyConsentedTabData, verifyContestedTabData, verifyContestedPaperTabData, verifyContestedConfidentialTabData } = require('./pages/tab-data-verification/tab-data-verification');
 const { assignContestedCase } = require('./pages/org-assign-cases/org-assign-cases-to-respondent');
 const { assignContestedShareCase } = require('./pages/share-case-in-org/share-case-in-org');
 const {contestedCaseworkerCreate} = require('./pages/caseworker-create/caseworker-create');
@@ -34,6 +34,9 @@ const {pbaPayment} = require('./pages/pba-payment/pba-payment');
 const {manualPayment} = require('./pages/manual-payment/manual-payment');
 const {issueApplication} = require('./pages/issue-application/issue-application');
 const {childrenDetails} = require('./pages/children-details/children-details');
+const {allocateJudge} = require('./pages/allocate-judge/allocate-judge');
+const {uploadCaseFiles} = require('./pages/upload-case-files/upload-case-files');
+const {manageConfidentialDocuments} = require('./pages/manage-confidential-documents/manage-confidential-documents');
 const crossBrowser = process.env.TESTS_FOR_CROSS_BROWSER || 'false';
 const adminUserName = process.env.CCD_ADMIN_USER_NAME
 const adminPassword = process.env.CCD_ADMIN_PASSWORD
@@ -63,16 +66,22 @@ module.exports = () => {
       this.click('Submit');
     },
     signInIdam(username, password) {
-      this.amOnPage(`${process.env.CCD_WEB_URL}`);
+       this.amOnPage(`${process.env.CCD_WEB_URL}`);
       this.wait('5');
       if(crossBrowser=='true'){
         this.wait('10');
       }
-      this.waitForText('Email address',50);
+      this.waitForText('Email address',60);
       this.fillField('username', username);
       this.fillField('password', password);
       this.click('Sign in');
       this.wait('15');
+    },
+
+    signOut() {
+      this.amOnPage(`${process.env.CCD_WEB_URL}`);
+      this.wait('5');
+      this.click('Sign out');
     },
     signInXuiOrg(username, password) {
       this.amOnPage(`${process.env.XUI_ORG_WEB_URL}`);
@@ -143,6 +152,10 @@ module.exports = () => {
     manualPayment,
     issueApplication,
     childrenDetails,
-    contestedNatureOfApplicationForSchedule1
+    contestedNatureOfApplicationForSchedule1,
+    allocateJudge,
+    uploadCaseFiles,
+    manageConfidentialDocuments,
+    verifyContestedConfidentialTabData
   });
 };
