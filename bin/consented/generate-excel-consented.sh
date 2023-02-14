42a16b036129d2aef57b897d348d71190b6bb4fb
srochani <sunil.rochani@hotmail.com>
2023-02-14 17:13:33 +0000

    DFR-1776: FR - Upgrade to Yarn v3

--- bin/consented/generate-excel-consented.sh
+++ bin/consented/generate-excel-consented.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CCD_DEF_CASE_TYPE_ID=FinancialRemedyMVP2 && pushd ccd-definition-processor && yarn --cwd ccd-definition-processor json2xlsx -D ../definitions/consented/json -e ${EXCLUDE} -o ../definitions/consented/xlsx/ccd-config-${FR_ENV:-base}-consented-${GIT_COMMIT:-base}.xlsx && popd
+pushd ccd-definition-processor && CCD_DEF_CASE_TYPE_ID=FinancialRemedyMVP2 yarn --cwd ccd-definition-processor json2xlsx -D ../definitions/consented/json -e ${EXCLUDE} -o ../definitions/consented/xlsx/ccd-config-${FR_ENV:-base}-consented-${GIT_COMMIT:-base}.xlsx && popd
--- bin/contested/generate-excel-contested.sh
+++ bin/contested/generate-excel-contested.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-CCD_DEF_CASE_TYPE_ID=FinancialRemedyContested && pushd ccd-definition-processor && yarn --cwd ccd-definition-processor json2xlsx -D ../definitions/contested/json -e ${EXCLUDE} -o ../definitions/contested/xlsx/ccd-config-${FR_ENV:-base}-contested-${GIT_COMMIT:-base}.xlsx && popd
+pushd ccd-definition-processor && CCD_DEF_CASE_TYPE_ID=FinancialRemedyContested yarn --cwd ccd-definition-processor json2xlsx -D ../definitions/contested/json -e ${EXCLUDE} -o ../definitions/contested/xlsx/ccd-config-${FR_ENV:-base}-contested-${GIT_COMMIT:-base}.xlsx && popd
