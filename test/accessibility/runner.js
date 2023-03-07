14eabeb8cc64082a9b858ede69357ce308989c56
srochani <sunil.rochani@hotmail.com>
2023-03-07 12:59:42 +0000

    DFR-1852: Fix crossbrowser and functioanl test for finrem-ccd-definition nightly build

--- test/accessibility/runner.js
+++ test/accessibility/runner.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 const HTMLCS = require('html_codesniffer');
 const fs = require('fs');
 // const testConfig = require('src/test/config.js');
@@ -14,27 +13,6 @@ const resultObj = {
   failCount: 0,
   tests: []
 };
-function updateResultObject(url, pageTitle, screenshotReportRef, accessibilityErrorsOnThePage) {
-  const isPageAccessible = accessibilityErrorsOnThePage.length === 0 ? result.PASSED : result.FAILED;
-
-  const urlArr = url.split('/');
-
-  if (isPageAccessible === result.PASSED) {
-    resultObj.passCount += 1;
-  } else {
-    resultObj.failCount += 1;
-  }
-
-  console.log('url -----------------------' + JSON.stringify(urlArr));
-  resultObj.tests.push({
-    name: `${urlArr[urlArr.length - 2]}/${urlArr[urlArr.length - 1]}`,
-    pageUrl: url,
-    documentTitle: pageTitle,
-    status: isPageAccessible,
-    screenshot: screenshotReportRef,
-    a11yIssues: accessibilityErrorsOnThePage
-  });
-}
 
 async function runAccessibility(url, page) {
   // Add HMTL code sniffer script
@@ -72,7 +50,7 @@ async function runAccessibility(url, page) {
 
   try {
     await page.screenshot({ path: `${screenshotPath}/${screenshotName}`, fullPage: true });
-  } catch (error) {
+  } catch (err) {
     fs.mkdirSync(screenshotPath, { recursive: true });
     await page.screenshot({ path: `${screenshotPath}/${screenshotName}`, fullPage: true });
   }
@@ -80,6 +58,26 @@ async function runAccessibility(url, page) {
   updateResultObject(url, await page.title(), screenshotReportRef, accessibilityErrorsOnThePage);
 }
 
+function updateResultObject(url, pageTitle, screenshotReportRef, accessibilityErrorsOnThePage) {
+  const isPageAccessible = accessibilityErrorsOnThePage.length === 0 ? result.PASSED : result.FAILED;
+
+  const urlArr = url.split('/');
+
+  if (isPageAccessible === result.PASSED) {
+    resultObj.passCount += 1;
+  } else {
+    resultObj.failCount += 1;
+  }
+
+  resultObj.tests.push({
+    name: `${urlArr[urlArr.length - 2]}/${urlArr[urlArr.length - 1]}`,
+    pageUrl: url,
+    documentTitle: pageTitle,
+    status: isPageAccessible,
+    screenshot: screenshotReportRef,
+    a11yIssues: accessibilityErrorsOnThePage
+  });
+}
 
 function getAccessibilityTestResult() {
   return resultObj;
