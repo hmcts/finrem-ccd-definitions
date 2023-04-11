fd35f87731a0b86423e2bac23119e4cf10eb12b3
Nilamkanojiya <nilam_kanojiya@yahoo.com>
2023-04-11 10:57:02 +0100

    fix test

--- test/functional/contested_test.js
+++ test/functional/contested_test.js
@@ -212,7 +212,7 @@ Scenario('Contested share case @nightly @pipeline', async I => {
 }).retry(3);
 
 
-Scenario('Contested Matrimonial Case Creation by Solicitor @nightly', async I => {
+Scenario('Contested Matrimonial Case Creation by Solicitor @nightly @test1', async I => {
     I.signInIdam(solicitorUserName, solicitorPassword);
     I.wait('2');
     await I.createCase('FinancialRemedyContested', 'Form A Application');
@@ -221,7 +221,6 @@ Scenario('Contested Matrimonial Case Creation by Solicitor @nightly', async I =>
     await I.contestedApplicantDetails();
     await I.contestedRespondentDetails();
     await I.contestedNatureOfApplication();
-    await I.contestedOrderForChildren();
     await I.fastTrack();
     await I.complexityList();
     await I.applyingToCourt();
@@ -310,7 +309,6 @@ Scenario('Contested Matrimonial Case Creation by Caseworker @nightly', async I =
     await I.contestedApplicantDetails();
     await I.contestedRespondentDetails();
     await I.contestedNatureOfApplication();
-    await I.contestedOrderForChildren();
     await I.fastTrack();
     await I.complexityList();
     await I.applyingToCourt();
@@ -335,7 +333,6 @@ Scenario('Upload Case Files (Confidential Documents) @nightly', async I => {
     await I.contestedApplicantDetails();
     await I.contestedRespondentDetails();
     await I.contestedNatureOfApplication();
-    await I.contestedOrderForChildren();
     await I.fastTrack();
     await I.complexityList();
     await I.applyingToCourt();
@@ -408,6 +405,22 @@ Scenario('Update Contact Details for contested Case @nightly ', async I => {
 
 }).retry(2);
 
+//
+// Scenario('Add Interveners @nightly', async I => {
+//     const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-contested-basic-data.json', 'FinancialRemedyContested', 'FR_solicitorCreate');
+//     const caseSubmission = await updateCaseInCcd(solicitorUserName, solicitorPassword, caseId, 'FinancialRemedyContested', 'FR_applicationPaymentSubmission', './test/data/ccd-hwf-contested-payment.json');
+//     const hwfPaymentAccepted = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyContested', 'FR_HWFDecisionMade', './test/data/ccd-contested-basic-data.json');
+//     const issueApplication = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyContested', 'FR_issueApplication', './test/data/ccd-contested-case-worker-issue-data.json');
+//
+//     await I.signInIdam(caseWorkerUserName, caseWorkerPassword);
+//     await I.amOnPage(`${ccdWebUrl}/v2/case/${caseId}`);
+//     I.wait('5');
+//     await I.manageInterveners();
+//     logger.info('Manage Interveners event completed');
+//     await I.contestedIntervenersTab(verifyTabText.historyTab.manageIntervenersEvent, verifyTabText.IntervenersTab);
+//     logger.info('Interveners tab verified');
+// }).retry(2);
+
 /*Scenario('Caseworker creates case flag  @nightly @pipeline', async I => {
 //case type - matrimonial
         //TODO- add API call to create case - end state should be application drafted
@@ -455,5 +468,7 @@ Scenario('Update Contact Details for contested Case @nightly ', async I => {
 }).retry(2);*/
 
 //GA and paper case -case flag
+
+
 
 
