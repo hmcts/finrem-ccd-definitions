b99f8b30172469d32ecb71077455f64b1a755a52
Johnny Vineall <vineall@live.co.uk>
2023-04-13 10:17:36 +0100

    Adding DFR-1953 Refund test

--- test/functional/contested_test.js
+++ test/functional/contested_test.js
@@ -424,6 +424,24 @@ Scenario('Add Interveners @nightly', async I => {
 }).retry(2);
 
 
+Scenario('Caseworker refunds an issued case @nightly', async I => {
+  logger.info("Refund test starting");
+
+  //Using API to create a case as a solicitor, and then issue case
+  const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-contested-basic-data.json', 'FinancialRemedyContested', 'FR_solicitorCreate');
+  const caseSubmission = await updateCaseInCcd(solicitorUserName, solicitorPassword, caseId, 'FinancialRemedyContested', 'FR_applicationPaymentSubmission', './test/data/ccd-hwf-contested-payment.json');
+  const hwfPaymentAccepted = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyContested', 'FR_HWFDecisionMade', './test/data/ccd-contested-basic-data.json');
+  const issueApplication = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyContested', 'FR_issueApplication', './test/data/ccd-contested-case-worker-issue-data.json');
+
+  //Signing in as a caseworker and navigating to the case
+  I.signInIdam(caseWorkerUserName, caseWorkerPassword);
+  I.amOnPage(`${ccdWebUrl}/v2/case/${caseId}`);
+
+  //Starting refundCase function
+  await I.refundCase()
+  logger.info("Refund test completed");
+})
+
 /*
 Scenario('Contested Schedule 1 Case Creation by Solicitor using API call @nightly', async I => {
    //TODO
