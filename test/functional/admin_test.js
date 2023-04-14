170fac925e1ccce8c1cb5771426fbcf3de3a0c10
rishisharma <rishi.sharma@hmcts.net>
2023-04-14 13:32:40 +0100

    DF979

--- test/functional/admin_test.js
+++ test/functional/admin_test.js
@@ -5,6 +5,7 @@ Scenario('add all the roles @pipeline', I => {
   I.createRole('citizen');
   I.createRole('caseworker');
   I.createRole('caseworker-divorce-financialremedy-courtadmin');
+  I.createRole('caseworker-divorce-financialremedy-solicitor');
   I.createRole('caseworker-divorce-financialremedy-judiciary');
   I.createRole('caseworker-divorce-systemupdate');
   I.createRole('caseworker-divorce-bulkscan');
@@ -12,29 +13,18 @@ Scenario('add all the roles @pipeline', I => {
   I.createRole('caseworker-caa');
   I.createRole('caseworker-divorce-financialremedy-superuser');
   I.createRole('caseworker-approver');
-  I.createRole('[APPSOLICITOR]');
-  I.createRole('[RESPSOLICITOR]');
-  I.createRole('[INTVRSOLICITOR1]');
-  I.createRole('[INTVRSOLICITOR2]');
-  I.createRole('[INTVRSOLICITOR3]');
-  I.createRole('[INTVRSOLICITOR4]');
   I.click('Manage User Roles');
   I.see('citizen');
   I.see('caseworker');
   I.see('caseworker-divorce-financialremedy-courtadmin');
+  I.see('caseworker-divorce-financialremedy-solicitor');
   I.see('caseworker-divorce-financialremedy-judiciary');
   I.see('caseworker-divorce-systemupdate');
   I.see('caseworker-divorce-bulkscan');
   I.see('caseworker-divorce-financialremedy');
   I.see('caseworker-caa');
-  I.see('caseworker-divorce-financialremedy-superuser');
+  I.see('caseworker-divorce-financialremedy-superuser')
   I.see('caseworker-approver');
-  I.createRole('[APPSOLICITOR]');
-  I.createRole('[RESPSOLICITOR]');
-  I.createRole('[INTVRSOLICITOR1]');
-  I.createRole('[INTVRSOLICITOR2]');
-  I.createRole('[INTVRSOLICITOR3]');
-  I.createRole('[INTVRSOLICITOR4]');
 }).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
 
 if (process.env.IMPORT_PREVIEW) {
