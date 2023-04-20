07fb77d0e9cd2c7ae82b72f546a50b99df2a6f84
Johnny Vineall <vineall@live.co.uk>
2023-04-20 09:51:37 +0100

    Moving to new general application test file

--- test/functional/contested_events_test.js
+++ test/functional/contested_events_test.js
@@ -193,20 +193,3 @@ Scenario('Contested E2E @nightly', async I => {
     await I.sendOrder();
     logger.info('-----------completed E2E contested test for -------------', caseId);
 }).retry(2);
-
-
-//Hearing not required currently - modify so hearing is required and supporting document added
-Scenario('Solicitor creates a general application with no hearing required @Johnny', async I => {
-    logger.info("General application test starting");
-    const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-contested-basic-data.json', 'FinancialRemedyContested', 'FR_solicitorCreate');
-    const caseSubmission = await updateCaseInCcd(solicitorUserName, solicitorPassword, caseId, 'FinancialRemedyContested', 'FR_applicationPaymentSubmission', './test/data/ccd-hwf-contested-payment.json');
-    const hwfPaymentAccepted = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyContested', 'FR_HWFDecisionMade', './test/data/ccd-contested-basic-data.json');
-    const issueApplication = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyContested', 'FR_issueApplication', './test/data/ccd-contested-case-worker-issue-data.json');
-
-    I.signInIdam(solicitorUserName, solicitorPassword);
-    I.amOnPage(`${ccdWebUrl}/v2/case/${caseId}`);
-    await I.solicitorCreateGeneralApplication();
-    await I.verifyGeneralApplicationTab();
-
-    logger.info("General application test completed");
-})
---
+++ test/functional/general_application_test.js
@@ -0,0 +1,36 @@
+const { createCaseInCcd, updateCaseInCcd, createSolicitorReference, createCaseworkerReference } = require('../helpers/utils');
+const verifyTabText = require('../data/verify-contested-tab-data.json');
+const verifyContestedPaperTabText = require('../data/verify-contested-paper-case-tab-data.json');
+const { Logger } = require('@hmcts/nodejs-logging');
+const logger = Logger.getLogger('helpers/utils.js');
+
+
+const ccdWebUrl = process.env.CCD_WEB_URL;
+const solicitorUserName = process.env.USERNAME_SOLICITOR;
+const solicitorPassword = process.env.PASSWORD_SOLICITOR;
+const caseWorkerUserName = process.env.USERNAME_CASEWORKER;
+const caseWorkerPassword = process.env.PASSWORD_CASEWORKER;
+const judgeUserName = process.env.USERNAME_JUDGE;
+const judgePassword = process.env.PASSWORD_JUDGE;
+const nightlyTest = process.env.NIGHTLY_TEST;
+const solRef = `AUTO-${createSolicitorReference()}`;
+const caRef= `AUTO-${createCaseworkerReference()}`;
+const runningEnv = process.env.RUNNING_ENV;
+
+Feature('General Application Flow');
+
+//Hearing not required currently - modify so hearing is required and supporting document added
+Scenario('Solicitor creates a general application with no hearing required @Johnny', async I => {
+    logger.info("General application test starting");
+    const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-contested-basic-data.json', 'FinancialRemedyContested', 'FR_solicitorCreate');
+    const caseSubmission = await updateCaseInCcd(solicitorUserName, solicitorPassword, caseId, 'FinancialRemedyContested', 'FR_applicationPaymentSubmission', './test/data/ccd-hwf-contested-payment.json');
+    const hwfPaymentAccepted = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyContested', 'FR_HWFDecisionMade', './test/data/ccd-contested-basic-data.json');
+    const issueApplication = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyContested', 'FR_issueApplication', './test/data/ccd-contested-case-worker-issue-data.json');
+
+    I.signInIdam(solicitorUserName, solicitorPassword);
+    I.amOnPage(`${ccdWebUrl}/v2/case/${caseId}`);
+    await I.solicitorCreateGeneralApplication();
+    await I.verifyGeneralApplicationTab();
+
+    logger.info("General application test completed");
+})
