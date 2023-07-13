0d988fc5cb5dd13b670ed907ed7c9c3b6ea54471
jamiephillips <jamie.phillips@justice.gov.uk>
2023-07-13 07:55:44 +0100

    DFR-2030 Update nightly test

--- test/functional/admin_test.js
+++ test/functional/admin_test.js
@@ -1,32 +1,32 @@
-Feature('Admin Web');
-
-Scenario('add all the roles @pipeline', I => {
-  I.loginToAdminConsole();
-  I.createRole('citizen');
-  I.createRole('caseworker');
-  I.createRole('caseworker-divorce-financialremedy-courtadmin');
-  I.createRole('caseworker-divorce-financialremedy-solicitor');
-  I.createRole('caseworker-divorce-financialremedy-judiciary');
-  I.createRole('caseworker-divorce-systemupdate');
-  I.createRole('caseworker-divorce-bulkscan');
-  I.createRole('caseworker-divorce-financialremedy');
-  I.createRole('caseworker-caa');
-  I.createRole('caseworker-divorce-financialremedy-superuser');
-  I.createRole('caseworker-approver');
-  I.click('Manage User Roles');
-  I.see('citizen');
-  I.see('caseworker');
-  I.see('caseworker-divorce-financialremedy-courtadmin');
-  I.see('caseworker-divorce-financialremedy-solicitor');
-  I.see('caseworker-divorce-financialremedy-judiciary');
-  I.see('caseworker-divorce-systemupdate');
-  I.see('caseworker-divorce-bulkscan');
-  I.see('caseworker-divorce-financialremedy');
-  I.see('caseworker-caa');
-  I.see('caseworker-divorce-financialremedy-superuser')
-  I.see('caseworker-approver');
-}).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
-
+// Feature('Admin Web');
+//
+// Scenario('add all the roles @pipeline', I => {
+//   I.loginToAdminConsole();
+//   I.createRole('citizen');
+//   I.createRole('caseworker');
+//   I.createRole('caseworker-divorce-financialremedy-courtadmin');
+//   I.createRole('caseworker-divorce-financialremedy-solicitor');
+//   I.createRole('caseworker-divorce-financialremedy-judiciary');
+//   I.createRole('caseworker-divorce-systemupdate');
+//   I.createRole('caseworker-divorce-bulkscan');
+//   I.createRole('caseworker-divorce-financialremedy');
+//   I.createRole('caseworker-caa');
+//   I.createRole('caseworker-divorce-financialremedy-superuser');
+//   I.createRole('caseworker-approver');
+//   I.click('Manage User Roles');
+//   I.see('citizen');
+//   I.see('caseworker');
+//   I.see('caseworker-divorce-financialremedy-courtadmin');
+//   I.see('caseworker-divorce-financialremedy-solicitor');
+//   I.see('caseworker-divorce-financialremedy-judiciary');
+//   I.see('caseworker-divorce-systemupdate');
+//   I.see('caseworker-divorce-bulkscan');
+//   I.see('caseworker-divorce-financialremedy');
+//   I.see('caseworker-caa');
+//   I.see('caseworker-divorce-financialremedy-superuser')
+//   I.see('caseworker-approver');
+// }).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
+//
 // if (process.env.IMPORT_PREVIEW) {
 //   Scenario('upload Consented preview config file @pipeline', I => {
 //     I.loginToAdminConsole();
