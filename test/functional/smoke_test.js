c9f6ed2bf534a77c7ee99cb13c371491e55c04e2
Luciano <luciano.marsilio@hmcts.net>
2023-07-19 11:04:41 +0100

    creating-smoke-test

---
+++ test/functional/smoke_test.js
@@ -0,0 +1,13 @@
+const { Logger } = require('@hmcts/nodejs-logging');
+const logger = Logger.getLogger('helpers/utils.js');
+
+const ccdWebUrl = process.env.CCD_WEB_URL;
+
+Feature('Smoke Test');
+
+Scenario('Check env running @smoke', async I => {
+  await I.amOnPage(`${process.env.CCD_WEB_URL}`);
+  await I.wait('5');
+  await I.waitForText('Sign in',60);
+  await I.see("Log in with your eJudiciary account");
+}).retry(3);
