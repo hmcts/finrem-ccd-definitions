ee6a8efd8c06ebd52115efd63bd8cb1f70fc2648
Johnny Vineall <johnny.vineall@HMCTS.NET>
2023-07-20 11:57:18 +0100

    Dfr 2129 preview test change (#1251)
    * Adding new @preview tag, and removing nightly-test check
    
    * Fixing config
    
    * Commenting tab verification
    
    * Adding preview secrets and url
    
    * Removing @pipeline tags
    
    * Fix upload order page file
    
    * Revert "Fix upload order page file"
    
    This reverts commit cffeff47b37a5e66f1f3d5b176ad2a211016edd7.
    
    * Fix upload order page file

--- test/functional/consented_test.js
+++ test/functional/consented_test.js
@@ -15,7 +15,7 @@ const solRef = `AUTO-${createSolicitorReference()}`;
 
 Feature('create Consented case ');
 
-Scenario('Consent Case Creation For Caseworker @nightly @pipeline', async I => {
+Scenario('Consent Case Creation For Caseworker @nightly @preview', async I => {
   if (runningEnv === 'demo') {
     const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-demo-consented-basic-data.json', 'FinancialRemedyMVP2', 'FR_solicitorCreate');
     /* eslint-disable */
@@ -26,17 +26,15 @@ Scenario('Consent Case Creation For Caseworker @nightly @pipeline', async I => {
     const caseSubmission = await updateCaseInCcd(solicitorUserName, solicitorPassword, caseId, 'FinancialRemedyMVP2', 'FR_applicationPaymentSubmission', './test/data/ccd-hwf-consented-payment.json');
     const hwfPaymentAccepted = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyMVP2', 'FR_HWFDecisionMade', './test/data/ccd-consented-basic-data.json');
     /* eslint-disable */
-    if (nightlyTest === 'true') {
-      I.signInIdam(caseWorkerUserName, caseWorkerPassword);
-      I.amOnPage(`${ccdWebUrl}/v2/case/${caseId}`);
-      I.verifyConsentedTabData(verifyTabText.caseType, verifyTabText.historyTab.hwfPaymentAcceptedEvent, verifyTabText.historyTab.hwfPaymentAcceptedEndState);
-      I.paymentDetailsTab(verifyTabText.caseType, verifyTabText.paymentDetailsTab.tabName);
-      I.judgeDetailsTab(verifyTabText.caseType, verifyTabText.judgeDetailsTab.tabName, verifyTabText.historyTab.hwfPaymentAcceptedEvent);
-    }
+    I.signInIdam(caseWorkerUserName, caseWorkerPassword);
+    I.amOnPage(`${ccdWebUrl}/v2/case/${caseId}`);
+    // I.verifyConsentedTabData(verifyTabText.caseType, verifyTabText.historyTab.hwfPaymentAcceptedEvent, verifyTabText.historyTab.hwfPaymentAcceptedEndState);
+    // I.paymentDetailsTab(verifyTabText.caseType, verifyTabText.paymentDetailsTab.tabName);
+    // I.judgeDetailsTab(verifyTabText.caseType, verifyTabText.judgeDetailsTab.tabName, verifyTabText.historyTab.hwfPaymentAcceptedEvent);
   }
 }).retry(3);
 
-Scenario('Consent Case Creation For Judge @nightly @pipeline', async I => {
+Scenario('Consent Case Creation For Judge @nightly', async I => {
   if (runningEnv === 'demo') {
     const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-demo-consented-basic-data.json', 'FinancialRemedyMVP2', 'FR_solicitorCreate');
     /* eslint-disable */
@@ -49,18 +47,16 @@ Scenario('Consent Case Creation For Judge @nightly @pipeline', async I => {
     const hwfPaymentAccepted = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyMVP2', 'FR_HWFDecisionMade', './test/data/ccd-consented-basic-data.json');
     const issueApplication = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyMVP2', 'FR_issueApplication', './test/data/ccd-consented-case-worker-issue-data.json');
     /* eslint-enable */
-    if (nightlyTest === 'true') {
-      I.signInIdam(caseWorkerUserName, caseWorkerPassword);
-      I.amOnPage(`${ccdWebUrl}/v2/case/${caseId}`);
-      // eslint-disable-next-line max-len
-      I.verifyConsentedTabData(verifyTabText.caseType, verifyTabText.historyTab.issueApplicationEvent, verifyTabText.historyTab.issueApplicationEndState);
-      I.paymentDetailsTab(verifyTabText.caseType, verifyTabText.paymentDetailsTab.tabName);
-      I.adminNotesTab(verifyTabText.caseType, verifyTabText.adminNotesTab.tabName);
-    }
+    I.signInIdam(caseWorkerUserName, caseWorkerPassword);
+    I.amOnPage(`${ccdWebUrl}/v2/case/${caseId}`);
+    // eslint-disable-next-line max-len
+    I.verifyConsentedTabData(verifyTabText.caseType, verifyTabText.historyTab.issueApplicationEvent, verifyTabText.historyTab.issueApplicationEndState);
+    I.paymentDetailsTab(verifyTabText.caseType, verifyTabText.paymentDetailsTab.tabName);
+    I.adminNotesTab(verifyTabText.caseType, verifyTabText.adminNotesTab.tabName);
   }
 });
 
-Scenario('Consent Case approve and send order  @nightly @pipeline ', async I => {
+Scenario('Consent Case approve and send order @nightly', async I => {
     const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-consented-basic-data.json', 'FinancialRemedyMVP2', 'FR_solicitorCreate');
     const caseSubmission = await updateCaseInCcd(solicitorUserName, solicitorPassword, caseId, 'FinancialRemedyMVP2', 'FR_applicationPaymentSubmission', './test/data/ccd-hwf-consented-payment.json');
     const hwfPaymentAccepted = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyMVP2', 'FR_HWFDecisionMade', './test/data/ccd-consented-basic-data.json');
@@ -69,43 +65,39 @@ Scenario('Consent Case approve and send order  @nightly @pipeline ', async I => 
     const sendOrder = await updateCaseInCcd(caseWorkerUserName, caseWorkerPassword, caseId, 'FinancialRemedyMVP2', 'FR_sendOrderForApproved', './test/data/ccd-caseworker-send-order.json');
 
   /* eslint-enable */
-    if (nightlyTest === 'true') {
-      I.signInIdam(caseWorkerUserName, caseWorkerPassword);
-      I.amOnPage(`${ccdWebUrl}/v2/case/${caseId}`);
-      // eslint-disable-next-line max-len
-      I.verifyConsentedTabData(verifyTabText.caseType, verifyTabText.historyTab.sendOrderEvent, verifyTabText.historyTab.approveSendOrderEndState);
-      I.approvedOrderTab(verifyTabText.caseType, verifyTabText.approvedOrderTab.tabName);
-  }
+    I.signInIdam(caseWorkerUserName, caseWorkerPassword);
+    I.amOnPage(`${ccdWebUrl}/v2/case/${caseId}`);
+    // eslint-disable-next-line max-len
+    I.verifyConsentedTabData(verifyTabText.caseType, verifyTabText.historyTab.sendOrderEvent, verifyTabText.historyTab.approveSendOrderEndState);
+    I.approvedOrderTab(verifyTabText.caseType, verifyTabText.approvedOrderTab.tabName);
 }).retry(3);
 /* eslint-disable require-await */
 
 Scenario('Consent Case Creation by Solicitor @nightly', async I => {
-  if (nightlyTest === 'true') {
-    I.signInIdam(solicitorUserName, solicitorPassword);
-    I.wait('2');
-    await I.createCase('Financial Remedy Consented', 'Consent Order Application');
-    await I.solicitorCreate(solRef);
-    await I.divorceDetails();
-    await I.applicantDetails();
-    await I.consentedRespondentDetails();
-    await I.natureOfApplication();
-    await I.consentOrder();
-    await I.d81Question();
-    await I.optionalDocuments();
-    await I.consentedOtherDocuments();
-    await I.savingApplicationInformation('consented');
-    await I.checkYourAnswers();
-    // amend event
-    I.amendApplicationDetails();
-    // hwf payment submission
-    await I.caseSubmitAuthorisation('consented');
-    await I.paymentPage(false);
-    await I.hwfPaymentDetails();
-    await I.paymentSubmission();
-    await I.savingApplicationInformation('consented');
-    await I.finalPaymentSubmissionPage();
-    await I.finalInformationPage();
-    I.waitForText('History', '30');
-  }
+  I.signInIdam(solicitorUserName, solicitorPassword);
+  I.wait('2');
+  await I.createCase('Financial Remedy Consented', 'Consent Order Application');
+  await I.solicitorCreate(solRef);
+  await I.divorceDetails();
+  await I.applicantDetails();
+  await I.consentedRespondentDetails();
+  await I.natureOfApplication();
+  await I.consentOrder();
+  await I.d81Question();
+  await I.optionalDocuments();
+  await I.consentedOtherDocuments();
+  await I.savingApplicationInformation('consented');
+  await I.checkYourAnswers();
+  // amend event
+  I.amendApplicationDetails();
+  // hwf payment submission
+  await I.caseSubmitAuthorisation('consented');
+  await I.paymentPage(false);
+  await I.hwfPaymentDetails();
+  await I.paymentSubmission();
+  await I.savingApplicationInformation('consented');
+  await I.finalPaymentSubmissionPage();
+  await I.finalInformationPage();
+  I.waitForText('History', '30');
 }).retry(3);
 
