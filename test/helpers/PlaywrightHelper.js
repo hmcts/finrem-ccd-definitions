b29552b006970a348149bd8de097cfd743e893de
Johnny Vineall <johnny.vineall@HMCTS.NET>
2023-11-13 11:25:48 +0000

    Dfr 2434 playwright migration (#1440)
    * Smoke testing working
    
    * Smoke testing working
    
    * Changing to new codecept syntax
    
    * Tab selection I think working
    
    * Fixing mochawesome reporter
    
    * Fixing build
    
    * Fixing build?
    
    * 3rd time lucky???
    
    * fix build
    
    * fix build
    
    * Downgrading mocha version
    
    * Removing module thing
    
    * Build fix?
    
    * Build fix
    
    * DFR-2434-playwright-migration - removed flat package
    
    * Yarn playwright install before smoke test
    
    * Testing before smoketest
    
    * Adding preview selector
    
    * Correcting codecept config for pipeline
    
    * Fixing config
    
    * Fix definition upload test
    
    * Cleaning Jekins CNP and adding flat to resolutions
    
    * Removing flat from resolutions again
    
    * Fixing accidental change
    
    * Enabling all tests in preview
    
    * Fixing grep
    
    * Removing spammy log message
    
    * Build fix
    
    * Reverting test tags, and fixing reporter
    
    * Removing commented code and whitespace
    
    ---------
    
    Co-authored-by: rishisharma <rishi.sharma@hmcts.net>
    Co-authored-by: rishikrsharma <rishikrsharma@users.noreply.github.com>

--- test/helpers/PlaywrightHelper.js
+++ test/helpers/PlaywrightHelper.js
@@ -2,14 +2,14 @@
 'use strict';
 
 const Helper = codecept_helper;
-const helperName = 'Puppeteer';
+const helperName = 'Playwright';
 const testForAccessibility = process.env.TESTS_FOR_ACCESSIBILITY || 'true';
 const { getAccessibilityTestResult } = require('../accessibility/runner');
 const { generateAccessibilityReport } = require('../reporter/customReporter');
 
 const { runAccessibility } = require('../accessibility/runner');
 
-class PuppeteerHelper extends Helper {
+class PlaywrightHelper extends Helper {
   async runAccessibilityTest() {
     if (!testForAccessibility) {
       return;
@@ -40,13 +40,15 @@ class PuppeteerHelper extends Helper {
 
   async clickTab(tabTitle) {
     const page = this.helpers[helperName].page;
+    const loopMax = 10;
     const tabXPath = `//div[text()='${tabTitle}']`;
-    const tabExists = await page.waitForXPath(tabXPath, {timeout: 6000}) ? true : false;
-    if (tabExists) {
-      const clickableTab = await page.$x(tabXPath);
-      await page.evaluate(el => {return el.click();}, clickableTab[0]);
+
+    for (let i = 1; i <loopMax; i++) {
+      tryTo(() => page.click(tabXPath));
+      await page.click('.mat-tab-header-pagination-after  .mat-tab-header-pagination-chevron');
     }
+
   }
 }
 
-module.exports = PuppeteerHelper;
+module.exports = PlaywrightHelper;
