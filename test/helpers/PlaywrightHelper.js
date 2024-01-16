0203ce29373d8a94b9cfa38d7dafb0fd00f7c6a1
srochani <sunil.rochani@hotmail.com>
2024-01-16 11:18:54 +0000

    Fix tests

--- test/helpers/PlaywrightHelper.js
+++ test/helpers/PlaywrightHelper.js
@@ -25,29 +25,106 @@ class PlaywrightHelper extends Helper {
     generateAccessibilityReport(getAccessibilityTestResult());
   }
 
-  async waitForNavigationToComplete(locator) {
+  async clickBrowserBackButton() {
     const page = this.helpers[helperName].page;
-    await page.waitForSelector(locator, {visible: true});
-    await page.click(locator);
+    await page.goBack();
   }
 
-  async getCaseRef() {
+  async delay(time) {
+    await new Promise((resolve => {
+      setTimeout(resolve, time * 1000);
+    }));
+  }
+
+  isXuiLink(locator) {
+    return locator.indexOf('href') >= 0 || locator.indexOf('navigation') >= 0;
+  }
+
+  adjustLocator(locator) {
+    return this.isXuiLink(locator) ? locator : `${locator}:enabled`;
+  }
+
+  // gets a locator from a locator which may be string or object with css property
+  getEnabledCssLocator(locator) {
+    if (!locator) {
+      return null;
+    }
+    return typeof locator === 'string' ? this.adjustLocator(locator) : this.adjustLocator(locator.css);
+  }
+
+  /*
+  async addATemporaryDummyTab() {
+      if (this.helpers[helperName].browser.newPage) {
+          // is Puppeteer. With Xui we have an issue where it gets stuck unless you open a new tab for some reason
+          const dummyTab = await this.helpers[helperName].browser.newPage();
+          await this.delay(0.2);
+          await dummyTab.close();
+      }
+  }
+  */
+
+  async waitForNavigationToComplete(locator, delay = 0) {
     const page = this.helpers[helperName].page;
-    const text = await page.$("#undefined");
-    const caseRef = text.getText();
-    return caseRef;
+
+    await this.delay(delay);
+    const promises = [];
+    promises.push(page.waitForNavigation({ waitUntil: 'domcontentloaded' }));
+    if (locator) {
+      if (Array.isArray(locator)) {
+        for (let i = 0; i < locator.length; i++) {
+          // eslint-disable-next-line no-await-in-loop
+          await page.waitForSelector(this.getEnabledCssLocator(locator[i]), { visible: true, timeout: 5000 });
+          promises.push(page.click(locator[i]));
+        }
+      } else {
+        await page.waitForSelector(this.getEnabledCssLocator(locator), { visible: true, timeout: 5000 });
+        promises.push(page.click(locator));
+      }
+    }
+    await Promise.all(promises);
+    await this.delay(delay);
+    // await this.addATemporaryDummyTab();
   }
 
   async clickTab(tabTitle) {
-    const page = this.helpers[helperName].page;
-    const loopMax = 10;
-    const tabXPath = `//div[text()='${tabTitle}']`;
+    const helper = this.helpers[helperName];
+    const tabXPath = `//div[contains(text(),"${tabTitle}")]`;
 
-    for (let i = 1; i <loopMax; i++) {
-      tryTo(() => page.click(tabXPath));
-      await page.click('.mat-tab-header-pagination-after  .mat-tab-header-pagination-chevron');
+    // wait for element defined by XPath appear in page
+    await helper.page.waitForSelector(tabXPath);
+
+    // evaluate XPath expression of the target selector (it returns array of ElementHandle)
+    const clickableTabs = await helper.page.$$(tabXPath);
+
+    /* eslint-disable no-await-in-loop */
+    for (let i = 0; i < clickableTabs.length; i++) {
+      await helper.page.evaluate(el => {
+        return el.click();
+      }, clickableTabs[i]);
     }
+  }
 
+  replaceAll(string, search, replace) {
+    if (!string) {
+      return null;
+    }
+    return string.split(search).join(replace);
+  }
+
+  htmlEquals(html1, html2) {
+    if ((html1 && !html2) || (html2 && !html1)) {
+      return false;
+    }
+    if (!html1 && !html2) {
+      return true;
+    }
+    return this.replaceAll(this.replaceAll(this.replaceAll(html1, '-c16'), '-c17'), '-c18') ===
+        this.replaceAll(this.replaceAll(this.replaceAll(html2, '-c16'), '-c17'), '-c18');
+  }
+
+  async navigateToPage(url) {
+    await this.amOnPage(url);
+    await this.waitForNavigationToComplete();
   }
 }
 
