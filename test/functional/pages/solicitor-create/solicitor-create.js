8905ea02376d342029f0d62842f1f4a22861e114
srochani <sunil.rochani@hotmail.com>
2024-01-31 12:27:39 +0000

    Update solicitor-create.js

--- test/functional/pages/solicitor-create/solicitor-create.js
+++ test/functional/pages/solicitor-create/solicitor-create.js
@@ -11,24 +11,25 @@ async function solicitorCreate(reference) {
   I.fillField('input[id="solicitorName"]', 'Arrow');
   I.fillField('input[id="solicitorFirm"]', 'Abc Firm');
   I.fillField('input[id="solicitorReference"]', reference);
-  I.waitForText('Search for an organisation','60')
-  I.fillField('input[id="search-org-text"]', 'FRApplicantSolicitorFirm');
-  I.wait('5');
-  I.click('Select');
-  I.wait('5');
-  I.fillField('input[id="ApplicantOrganisationPolicy_OrgPolicyReference"]', 'FRApplicant');
   I.fillField('#solicitorAddress_solicitorAddress_postcodeInput', 'TW3 1SS');
   I.click('div[id="solicitorAddress_solicitorAddress_postcodeLookup"] button[type="button"]');
   I.waitForElement('#selectAddress', '30');
   I.wait('5');
-  I.selectOption('select[id="solicitorAddress_solicitorAddress_addressList"]', { label: '67 Pears Road, Hounslow' });
+  I.selectOption('Select an address', '67 Pears Road, Hounslow');
   I.fillField('input[id="solicitorAddress__detailAddressLine2"]', 'Near Roundabout');
   I.fillField('input[id="solicitorAddress__detailAddressLine3"]', 'Opposite Tesco');
   I.fillField('input[id="solicitorAddress__detailCounty"]', 'Middlesex');
   I.fillField('input[id="solicitorPhone"]', '07700000');
   I.fillField('input[id="solicitorEmail"]', 'fr_applicant_sol@sharklasers.com');
   I.fillField('input[id="solicitorDXnumber"]', '776890');
+  I.waitForText('Search for an organisation','60')
   I.checkOption('input[id="solicitorAgreeToReceiveEmails_Yes"]');
+  I.fillField('input[id="search-org-text"]', 'FRApplicantSolicitorFirm');
+  I.wait('5');
+  I.click('Select');
+  I.wait('5');
+  I.waitForText('SW1H 9AJ');
+  I.fillField('input[id="ApplicantOrganisationPolicy_OrgPolicyReference"]', 'FRApplicant');
   I.waitForContinueButtonEnabled();
   I.click('Continue');
 }
