98efd766fb5b0bedefe38da559feb2b0316a8b87
James Turnbull <james.turnbull@justice.gov.uk>
2024-03-17 12:09:02 +0000

    Remove CCD import code

--- test/functional/ccd_setup_test.js
+++ test/functional/ccd_setup_test.js
@@ -1,5 +1,4 @@
-const { createCcdRoles, importCcdConfig } = require('../helpers/ccd-config-utils');
-const assert = require('assert');
+const { createCcdRoles } = require('../helpers/ccd-config-utils');
 
 const adminUserName = process.env.CCD_ADMIN_USER_NAME;
 const adminPassword = process.env.CCD_ADMIN_PASSWORD;
@@ -9,17 +8,3 @@ Feature('CCD Config Setup');
 Scenario('add all the roles using CCD Definition Store API @preview', async () => {
     await createCcdRoles(adminUserName, adminPassword);
 }).retry({ retries: 1, minTimeout: 30000 });
-
-if (process.env.IMPORT_PREVIEW) {
-    Scenario('import Consented preview config file @preview', async () => {
-        let result = await importCcdConfig(adminUserName, adminPassword,
-            `../../definitions/consented/xlsx/ccd-config-preview-consented-${process.env.GIT_COMMIT}.xlsx`);
-        assert.equal(result, 'Case Definition data successfully imported');
-    }).retry({ retries: 1, minTimeout: 30000 });
-
-    Scenario('import Contested preview config file @preview', async () => {
-        let result = await importCcdConfig(adminUserName, adminPassword,
-            `../../definitions/contested/xlsx/ccd-config-preview-contested-${process.env.GIT_COMMIT}.xlsx`);
-        assert.equal(result, 'Case Definition data successfully imported');
-    }).retry({ retries: 1, minTimeout: 30000 });
-}
