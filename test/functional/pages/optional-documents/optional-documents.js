01609a260715d7202077327937191a451d027d28
AbuSalam22 <136581242+AbuSalam22@users.noreply.github.com>
2024-03-26 09:09:58 +0000

    Increase optional docs time

--- test/functional/pages/optional-documents/optional-documents.js
+++ test/functional/pages/optional-documents/optional-documents.js
@@ -27,7 +27,7 @@ async function consentedOtherDocuments() {
 
 async function contestedOtherDocuments() {
   const I = this;
-  I.waitForText('Do you want to upload any other documents ?', 5);
+  I.waitForText('Do you want to upload any other documents ?', 15);
   if (testForAccessibility=='true') {
     await I.runAccessibilityTest();
   }
