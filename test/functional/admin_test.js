29ce855089b3d8f3117190b7e2530e994cf8e709
James Turnbull <james.turnbull@justice.gov.uk>
2024-03-28 11:53:29 +0000

    Configure to use preview ccd definition store

--- test/functional/admin_test.js
+++ test/functional/admin_test.js
@@ -26,15 +26,3 @@ Scenario('add all the roles @preview', ({ I }) => {
   I.see('caseworker-divorce-financialremedy-superuser')
   I.see('caseworker-approver');
 }).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
-
-if (process.env.IMPORT_PREVIEW) {
-  Scenario('upload Consented preview config file @preview', ({ I }) => {
-    I.loginToAdminConsole();
-    I.uploadConfig(`../../definitions/consented/xlsx/ccd-config-preview-consented-${process.env.GIT_COMMIT}.xlsx`);
-  }).retry({retries: 3, minTimeout: 30000});
-
-  Scenario('upload Contested preview config file @preview', ({ I }) => {
-    I.loginToAdminConsole();
-    I.uploadConfig(`../../definitions/contested/xlsx/ccd-config-preview-contested-${process.env.GIT_COMMIT}.xlsx`);
-  }).retry({retries: 3, minTimeout: 300000})
-}
