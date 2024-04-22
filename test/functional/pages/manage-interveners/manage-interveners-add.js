aa0b06ac1b2e3ae3106d7bb9ab523ee1327b6cdd
Adam Lee <166380454+al-hmcts@users.noreply.github.com>
2024-04-22 10:06:53 +0100

    Fix nightly build Implement new solicitor1 for Casework add interveners (#1758)
    * Implement new solicitor1 for Casework add interveners
    
    * Remove @mytest
    
    * Revert intervener 1 to barrister

--- test/functional/pages/manage-interveners/manage-interveners-add.js
+++ test/functional/pages/manage-interveners/manage-interveners-add.js
@@ -27,7 +27,7 @@ async function manageIntervenersAdd(flag){
         I.fillField('input[id="intervener1_intervenerSolName"]', 'Miranda Wiley');
         I.fillField('input[id="intervener1_intervenerSolicitorFirm"]', 'FR Intervener');
         I.fillField('input[id="intervener1_intervenerSolicitorReference"]', '234');
-        I.fillField('input[id="intervener1_intervenerSolEmail"]', 'fr_intervener1_solicitor@mailinator.com');
+        I.fillField('input[id="intervener1_intervenerSolEmail"]', 'fr_applicant_barrister1@mailinator.com');
         I.waitForText('Search for an organisation','30');
         I.fillField('input[id="search-org-text"]', 'FinRem-1-Org');
         I.click('Select');
