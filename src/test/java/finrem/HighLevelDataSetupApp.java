d8026a3c193391034b4ec8b7d40e3eee0437c14d
James Turnbull <james.turnbull@justice.gov.uk>
2024-07-12 18:42:10 +0100

    Add access profile

--- src/test/java/finrem/HighLevelDataSetupApp.java
+++ src/test/java/finrem/HighLevelDataSetupApp.java
@@ -23,7 +23,7 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
             new CcdRoleConfig("caseworker-divorce-bulkscan", "PUBLIC"),
             new CcdRoleConfig("caseworker-divorce-financialremedy", "PUBLIC"),
             new CcdRoleConfig("caseworker-caa", "PUBLIC"),
-            new CcdRoleConfig("caseworker-caa", "PUBLIC")
+            new CcdRoleConfig("caseworker-approver", "PUBLIC")
     );
 
     /**
