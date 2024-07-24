2c7ba1cf531f2d083a18cb66eb133d2ebe419dc7
James Turnbull <james.turnbull@justice.gov.uk>
2024-07-24 08:57:30 +0100

    Make local dev config scripts consistent with other environments

--- bin/consented/generate-excel-local-consented.sh
+++ bin/consented/generate-excel-local-consented.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source default.env
-FR_ENV=local CCD_DEF_COS_URL=http://localhost:9000 CCD_DEF_AAC_URL=http://localhost:4454 CCD_DEF_CCD_URL=$npm_package_config_local_ccdUrl EXCLUDE='*-prod.json' yarn run generate-excel-consented
+FR_ENV=local CCD_DEF_COS_URL=$npm_package_config_local_cosUrl CCD_DEF_AAC_URL=$npm_package_config_local_aacUrl CCD_DEF_CCD_URL=$npm_package_config_local_ccdUrl EXCLUDE='*-prod.json' yarn run generate-excel-consented
