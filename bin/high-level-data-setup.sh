335e2958e497536e813c63606c559bda32c17259
James Turnbull <james.turnbull@justice.gov.uk>
2024-09-12 11:53:30 +0100

    test

--- bin/high-level-data-setup.sh
+++ bin/high-level-data-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-java21Location=$("ls -d /usr/lib/jvm/temurin-21-jdk-*")
+java21Location=$("ls -d /usr/lib/jvm/*21*")
+echo "$java21Location"
 export JAVA_HOME=$java21Location
 export PATH=$JAVA_HOME/bin:$PATH
 
-./gradlew highLevelDataSetup --args=$1
-./after-data-setup-step.sh
+gradle highLevelDataSetup --args=$1
