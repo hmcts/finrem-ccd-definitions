ff5c5a297e6d7cad8a7528415b5894ca2f47abc8
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2024-09-12 11:16:30 +0100

    Update admin_ccd_definitions_import_test.js

--- test/functional/admin_ccd_definitions_import_test.js
+++ test/functional/admin_ccd_definitions_import_test.js
@@ -1,5 +1,32 @@
 Feature('Import CCD Definitions');
 
+Scenario('add all the roles @preview', ({ I }) => {
+  I.loginToAdminConsole();
+  I.createRole('citizen');
+  I.createRole('caseworker');
+  I.createRole('caseworker-divorce-financialremedy-courtadmin');
+  I.createRole('caseworker-divorce-financialremedy-solicitor');
+  I.createRole('caseworker-divorce-financialremedy-judiciary');
+  I.createRole('caseworker-divorce-systemupdate');
+  I.createRole('caseworker-divorce-bulkscan');
+  I.createRole('caseworker-divorce-financialremedy');
+  I.createRole('caseworker-caa');
+  I.createRole('caseworker-divorce-financialremedy-superuser');
+  I.createRole('caseworker-approver');
+  I.click('Manage User Roles');
+  I.see('citizen');
+  I.see('caseworker');
+  I.see('caseworker-divorce-financialremedy-courtadmin');
+  I.see('caseworker-divorce-financialremedy-solicitor');
+  I.see('caseworker-divorce-financialremedy-judiciary');
+  I.see('caseworker-divorce-systemupdate');
+  I.see('caseworker-divorce-bulkscan');
+  I.see('caseworker-divorce-financialremedy');
+  I.see('caseworker-caa');
+  I.see('caseworker-divorce-financialremedy-superuser')
+  I.see('caseworker-approver');
+}).retry({ retries: 3, minTimeout: 30000 }); // eslint-disable-line no-magic-numbers
+
 if (process.env.IMPORT_PREVIEW) {
     Scenario('upload Consented preview config file @preview', ({I}) => {
         I.loginToAdminConsole();
