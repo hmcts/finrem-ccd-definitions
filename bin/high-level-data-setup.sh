7d3b1b76b26ed40ad9d70240aed1d12591efebaf
James Turnbull <james.turnbull@justice.gov.uk>
2024-09-12 12:00:19 +0100

    test

--- bin/high-level-data-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-java21Location=$("ls -d /usr/lib/jvm/*21*")
-echo "$java21Location"
-export JAVA_HOME=$java21Location
-export PATH=$JAVA_HOME/bin:$PATH
-
-gradle highLevelDataSetup --args=$1
