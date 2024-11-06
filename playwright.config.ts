dc22ed0af551c8e9f6e6e1940a0cb2ddca78ebd7
Adam <adam.lee2@justice.gov.uk>
2024-11-06 15:12:53 +0000

    Only run against chrome and add line output

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,8 +16,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 5 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [[process.env.CI ? 'html' : 'list'],
-    ['html', { outputFolder: '../test-results/functionalTest' }]],
+  reporter: [
+    ['line'],
+    [process.env.CI ? 'html' : 'list'],
+    ['html', { outputFolder: '../test-results/functionalTest' }]
+  ],
   expect: { timeout: 1*110*1000 }, //wait time for the assertion to be true 110 sec
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -43,14 +46,14 @@ export default defineConfig({
     },
 
     /* Test against mobile viewports. */
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 7'] },
-    },
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 7'] },
+    // },
 
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
   ],
 });
