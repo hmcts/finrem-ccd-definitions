8ebf51b8cbd89772e5166a0f3e65cd099768d019
Adam <adam.lee2@justice.gov.uk>
2024-11-06 13:52:17 +0000

    update retries and timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,19 +8,17 @@ export default defineConfig({
   testMatch:'*spec.ts',
   /* Run tests in files in parallel */
   fullyParallel: true,
-  timeout: 100000,
+  timeout: 3*60*1000, //each test execution time is set to 3 min
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 3 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [[process.env.CI ? 'html' : 'list'],
     ['html', { outputFolder: '../test-results/functionalTest' }]],
-  expect: {
-    timeout: 10 * 3000,
-  },
+  expect: { timeout: 1*110*1000 }, //wait time for the assertion to be true 110 sec
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
