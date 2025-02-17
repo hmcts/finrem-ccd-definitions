364f18dda1716bc82b457854902df7c37034cedb
James Turnbull <james.turnbull@justice.gov.uk>
2025-02-17 15:59:46 +0000

    Add import method

--- bin/judges/validate.js
+++ bin/judges/validate.js
@@ -9,6 +9,18 @@ const otherFile = 'build/extracted.json';
 const combinedData = JSON.parse(fs.readFileSync(combinedFile, 'utf8'));
 const otherData = JSON.parse(fs.readFileSync(otherFile, 'utf8'));
 
+// Check for duplicate ListElementCode values
+const listElementCodes = otherData.map(item => item.ListElementCode);
+const duplicates = listElementCodes.filter((item, index) => listElementCodes.indexOf(item) !== index);
+
+if (duplicates.length > 0) {
+    console.error('Duplicate ListElementCode values found:');
+    console.error(JSON.stringify(duplicates, null, 2));
+    process.exit(1);
+} else {
+    console.log('No duplicate ListElementCode values found');
+}
+
 // Compare the combined data with the other data
 const additions = otherData
     .filter(item => {
@@ -18,7 +30,7 @@ const additions = otherData
     .map(item => `${item.ListElement} ${item.ListElementCode}`);
 
 if (additions.length > 0) {
-    console.log('Additions found:');
+    console.log('Additions found: ' + additions.length);
     console.log(JSON.stringify(additions, null, 2));
 } else {
     console.log('No additions found');
@@ -32,7 +44,7 @@ const changes = otherData
     .map(item => `${item.ListElement} ${item.ListElementCode}`);
 
 if (changes.length > 0) {
-    console.log('Changes found:');
+    console.log('Changes found: ' + changes.length);
     console.log(JSON.stringify(changes, null, 2));
 } else {
     console.log('No changes found');
