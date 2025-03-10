384aeba3ac8f6cfb444f03871ee61fbd0580519b
dawudgovuk <dawud.rahman@justice.gov.uk>
2025-03-10 12:49:28 +0000

    DFR-3610-Playwright-Tests-CS-C8-Refuge

--- playwright-e2e/pages/events/create-case/CreateCaseCheckYourAnswersPage.ts
+++ playwright-e2e/pages/events/create-case/CreateCaseCheckYourAnswersPage.ts
@@ -10,8 +10,8 @@ export class CreateCaseCheckYourAnswersPage extends BaseJourneyPage{
 
     public constructor(page: Page){
         super(page)
-        this.applicantInRefugeAnswer = page.getByRole('row', { name: 'Is the Applicant currently a' }).locator('span').nth(1);
-        this.respondentInRefugeAnswer = page.getByRole('row', { name: 'Is the Respondent currently a' }).locator('span').nth(1);
+        this.applicantInRefugeAnswer = page.getByRole('row', { name: 'Is the Applicant currently a resident in a refuge?' }).locator('span').nth(1);
+        this.respondentInRefugeAnswer = page.getByRole('row', { name: 'Is the Respondent currently a resident in a refuge?' }).locator('span').nth(1);
         this.netAssetsAnswer = page.getByRole('row', { name: 'Select one' }).locator('span').nth(1);
     }
 
