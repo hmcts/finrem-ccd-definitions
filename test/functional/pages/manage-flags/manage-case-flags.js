c970570ad2cde901a5fcd651452bb9869f1c04d5
tomfairclough1 <167980396+tomfairclough1@users.noreply.github.com>
2025-03-17 11:24:05 +0000

    DFR-3676: Amended create case flag and manage case flag to check for 'continue' button instead of 'next' (#2421)

--- test/functional/pages/manage-flags/manage-case-flags.js
+++ test/functional/pages/manage-flags/manage-case-flags.js
@@ -6,13 +6,13 @@ async function manageFlags(){
   I.click('Go');
   I.waitForText('Manage case flags', '30');
   I.checkOption('#flag-selection-0');
-  I.click('Next');
+  I.click('Continue');
   //I.waitForText('Update flag "Vulnerable user"', '30');
 
   I.click('Make inactive');
   const flagStatus = I.grabTextFrom('//*[@id="flag-status-container-v1"]/div[1]/p/span/strong');
   assert(flagStatus, 'INACTIVE');
-  I.click('Next');
+  I.click('Continue');
   I.waitForText('Submit');
   I.click('Submit');
   return flagStatus;
