879df848c5421271ddd4c9cc6c2aec34377675c5
Paul Trelease <paul.trelease@justice.gov.uk>
2025-03-20 11:59:58 +0000

    Amended guidance link and test

--- playwright-e2e/pages/events/create-case/ExpressCaseEnrolledPage.ts
+++ playwright-e2e/pages/events/create-case/ExpressCaseEnrolledPage.ts
@@ -14,20 +14,27 @@ export class ExpressCaseEnrolledPage extends BaseJourneyPage {
     }
 
     /**
-    * Checks the correct GOV.UK Express Case guidance URL is visible on the page.
-    * Opens the same URL in a new page, called GovUkGuidancePage.
-    * Checks that the expected content is showing on GovUkGuidancePage.
-    * Closes the GovUkGuidancePage when done.
+    * Click the link, which opens in a new tab.
+    * Checks that the expected content is showing on the new tab.
+    * Closes the tab when done.
     *
     * NOTE: Page not on GOV.UK yet, so looking for "Page not found".
     */
     async checkLinkResolves() {
-        // Check for correct URL on page.
+
         await expect(this.guidanceLink).toBeVisible();
-        // Go to the URL, in a new page, then check the content is correct.
-        const GovUkGuidancePage:Page = await this.page.context().newPage();
-        await GovUkGuidancePage.goto(this.guidanceLinkUrl);
-        await expect(GovUkGuidancePage.locator(`text=${this.guidancePageContent}`)).toBeVisible();
-        await GovUkGuidancePage.close();
+
+        // Click the link and capture the new tab that opens
+        const [newPage] = await Promise.all([
+            this.page.context().waitForEvent('page'),
+            this.guidanceLink.click()
+        ]);
+
+        // Wait for the HTML on the new tab to completely load
+        await newPage.waitForLoadState('domcontentloaded');
+
+        await expect(newPage.locator(`text=${this.guidancePageContent}`)).toBeVisible();
+
+        await newPage.close();
     }
 }
