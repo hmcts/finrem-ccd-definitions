87be57d6fc06761096185acf7aa0c04c175f9cca
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2025-03-27 11:49:09 +0000

    Express pilot does not qualify tab content

---
+++ playwright-e2e/data/tab_content/contested/express_case_not_qualify_gatekeeping_tab.ts
@@ -0,0 +1,7 @@
+export const expressDoesNotQualifyCaseGateKeepingTabData = [{
+    tabName: 'Gatekeeping and allocation',
+    tabContent: [
+      "Express Pilot Participation: Does not qualify",
+    ]
+  }
+]
