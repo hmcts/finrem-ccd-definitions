7f4970c04110ba14f8db4fbf818a491c347469cc
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2025-03-27 11:50:19 +0000

    e2e test on exiting express pilot through amend application

--- playwright-e2e/functional/contested/amend_application_details/amend_application_details_express_case.spec.ts
+++ playwright-e2e/functional/contested/amend_application_details/amend_application_details_express_case.spec.ts
@@ -1,20 +1,33 @@
 import { expect, test } from '../../../fixtures/fixtures';
 import config from '../../../config/config';
-import { createCaseInCcd, updateCaseInCcd } from '../../../../test/helpers/utils';
+import { createCaseInCcd } from '../../../../test/helpers/utils';
 import { contestedEvents } from '../../../config/case_events';
+import { expressDoesNotQualifyCaseGateKeepingTabData } from '../../../data/tab_content/contested/express_case_not_qualify_gatekeeping_tab';
+import { createCaseTabData } from '../../../data/tab_content/contested/solicitor_create_case_tabs';
+
+async function createAndProcessFormAExpressCase(): Promise<string> {
+  const caseId = await createCaseInCcd(config.applicant_solicitor.email, config.applicant_solicitor.password, './playwright-e2e/data/payload/contested/forma/ccd-contested-express-case-creation.json', 'FinancialRemedyContested', 'FR_solicitorCreate');
+  return caseId;
+}
 
 async function createAndProcessFormACase(): Promise<string> {
   const caseId = await createCaseInCcd(config.applicant_solicitor.email, config.applicant_solicitor.password, './playwright-e2e/data/payload/contested/forma/ccd-contested-express-case-creation.json', 'FinancialRemedyContested', 'FR_solicitorCreate');
   return caseId;
 }
 
-async function performAmendApplicationDetailsFlow(
+async function performAmendApplicationDetailsFlowToJoinExpressPilot(
   caseId: string,
   loginPage: any,
   manageCaseDashboardPage: any,
   caseDetailsPage: any,
   startPage: any,
-  SolicitorDetailsPage: any,
+  natureOfApplicationPage: any,
+  propertyAdjustmentPage: any,
+  periodicalPaymentsPage: any,
+  writtenAgreementPage: any,
+  expressCaseExitPage: any,
+  uploadOrderDocumentsPage: any,
+  createCaseCheckYourAnswersPage: any,
   testInfo: any,
   makeAxeBuilder: any
 ): Promise<void> {
@@ -25,12 +38,70 @@ async function performAmendApplicationDetailsFlow(
   await caseDetailsPage.selectNextStep(contestedEvents.amendApplicationDetails);
   await startPage.navigateContinue();
 
+  await startPage.navigateContinue();
+  await startPage.navigateContinue();
+  await startPage.navigateContinue();
+  await startPage.navigateContinue();
+  await startPage.navigateContinue();
+
+  // Nature of App - Select Variation order to exit Express Pilot
+  await natureOfApplicationPage.selectNatureOfApplication();
+  await natureOfApplicationPage.navigateContinue();
+
+  // Property Adjustment Order
+  await propertyAdjustmentPage.propertyAdjustmentOrder();
+  await propertyAdjustmentPage.addAdditionalPropertyAdjustment(true);
+  await propertyAdjustmentPage.navigateContinue();
+
+  // Periodical Payments
+  await periodicalPaymentsPage.selectPeriodicalPaymentsContested(true);
+  await periodicalPaymentsPage.navigateContinue();
+
+  // Written Agreement
+  await writtenAgreementPage.selectWrittenAgreement(false);
+  await writtenAgreementPage.navigateContinue();
   
+  await startPage.navigateContinue();
+  await startPage.navigateContinue();
+  await startPage.navigateContinue();
+
+  // Express Pilot exit page
+  await expressCaseExitPage.checkContent();
+  await expressCaseExitPage.navigateContinue();
+
+  await startPage.navigateContinue();
+  await startPage.navigateContinue();
+
+  // Upload variation Order Document
+  await uploadOrderDocumentsPage.uploadVariationOrderDoc();
+  await uploadOrderDocumentsPage.navigateContinue();
+  
+  await uploadOrderDocumentsPage.navigateContinue();
+  await createCaseCheckYourAnswersPage.navigateSubmit();
+
+  await caseDetailsPage.checkHasBeenUpdated('Amend Application Details');
+
+  // Assert case creation tab data
+  await caseDetailsPage.assertTabData(createCaseTabData);
+
+  // Assert tab data
+  await caseDetailsPage.assertTabData(expressDoesNotQualifyCaseGateKeepingTabData);
+
+  if (config.run_accessibility) {
+    const accessibilityScanResults = await makeAxeBuilder().analyze();
+
+    await testInfo.attach('accessibility-scan-results', {
+      body: JSON.stringify(accessibilityScanResults, null, 2),
+      contentType: 'application/json'
+    });
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+  }
 }
 
 test.describe('Contested - Amend Application Details express Form A', () => {
    test(
-    'Contested - Amend Application Details express case (Form A)',
+    'Contested - Amend Application Details exit Express Pilot through Variation Order (Form A)',
      { tag: [] },
      async (
        {
@@ -38,13 +109,21 @@ test.describe('Contested - Amend Application Details express Form A', () => {
          manageCaseDashboardPage,
          caseDetailsPage,
          startPage,
-         solicitorDetailsPage,
+         natureOfApplicationPage,
+         propertyAdjustmentPage,
+         periodicalPaymentsPage,
+         writtenAgreementPage,
+         expressCaseExitPage,
+         uploadOrderDocumentsPage,
+         createCaseCheckYourAnswersPage,
          makeAxeBuilder,
        },
        testInfo
      ) => {
-       const caseId = await createAndProcessFormACase();
-       await performAmendApplicationDetailsFlow(caseId, loginPage, manageCaseDashboardPage, caseDetailsPage, startPage, solicitorDetailsPage, testInfo, makeAxeBuilder);
+       const caseId = await createAndProcessFormAExpressCase();
+       await performAmendApplicationDetailsFlowToJoinExpressPilot(caseId, loginPage, manageCaseDashboardPage, caseDetailsPage, startPage, 
+        natureOfApplicationPage, propertyAdjustmentPage, periodicalPaymentsPage, writtenAgreementPage, expressCaseExitPage, uploadOrderDocumentsPage, 
+        createCaseCheckYourAnswersPage, testInfo, makeAxeBuilder);
      }
    );
 });
