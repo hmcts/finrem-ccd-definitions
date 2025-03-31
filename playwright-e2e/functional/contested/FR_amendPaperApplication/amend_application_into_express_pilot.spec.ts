1330d073b4eae88095482dc2b7085e473643ca63
Ashley Wong <ashley.wong@justice.gov.uk>
2025-03-31 15:26:04 +0100

    add test case for existing express pilot

--- playwright-e2e/functional/contested/FR_amendPaperApplication/amend_application_into_express_pilot.spec.ts
+++ playwright-e2e/functional/contested/FR_amendPaperApplication/amend_application_into_express_pilot.spec.ts
@@ -1,6 +1,9 @@
 import { test } from '../../../fixtures/fixtures';
 import config from '../../../config/config';
-import { createCaseWithEstimateAssetUnder1M, createCaseWithNonParticipatingFrcCourt } from '../../helpers/ExpressPilotHelper';
+import { 
+  createCaseWithEstimateAssetUnder1M,
+  createCaseWithNonParticipatingFrcCourt, 
+  createCaseWithExpressPilot } from '../../helpers/ExpressPilotHelper';
 import { contestedEvents } from '../../../config/case_events';
 
 test.describe('Contested - Paper Case - Amend application into Express Pilot', () => {
@@ -80,6 +83,46 @@ test.describe('Contested - Paper Case - Amend application into Express Pilot', (
       await amendApplicationDetailsPage.selectParticipatingCourt();
       await amendApplicationDetailsPage.navigateContinue(); 
       await amendApplicationDetailsPage.verifyDynamicEnrollmentMessageIsVisible();
+    }
+  );
+  test(
+    'Exiting express pilot message dynamic page should be inserted when changing total value of assets',
+    { tag: [] },
+    async (
+      {
+        loginPage,
+        manageCaseDashboardPage,
+        caseDetailsPage,
+        amendApplicationDetailsPage
+      }
+    ) => {
+      const caseId = await createCaseWithExpressPilot(
+        config.caseWorker.email,
+        config.caseWorker.password,
+        './playwright-e2e/data/payload/contested/paper_case/ccd-contested-base.json',
+        'FinancialRemedyContested',
+        'FR_newPaperCase'
+      );
+      await manageCaseDashboardPage.visit();
+      await loginPage.login(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL);
+      await manageCaseDashboardPage.navigateToCase(caseId);
+      await caseDetailsPage.assertTabData([{ tabName: 'Gatekeeping and allocation', tabContent: ['The net assets in this case are currently estimated to be in the order of Under £250,000 (this should be total of combined net assets, but excluding pensions)'] }]);
+    
+      await caseDetailsPage.selectNextStep(contestedEvents.amendApplicationDetails);
+      await amendApplicationDetailsPage.navigateContinue();
+      await amendApplicationDetailsPage.navigateContinue();
+      await amendApplicationDetailsPage.navigateContinue();
+      await amendApplicationDetailsPage.navigateContinue();
+      await amendApplicationDetailsPage.navigateContinue();
+      await amendApplicationDetailsPage.navigateContinue();
+      await amendApplicationDetailsPage.navigateContinue();
+      await amendApplicationDetailsPage.navigateContinue();
+      await amendApplicationDetailsPage.verifyEstimatedAssetsLabelIsVisible();
+      await amendApplicationDetailsPage.selectUnder1M();
+      await amendApplicationDetailsPage.enterEstimatedAssets('999999');
+      await amendApplicationDetailsPage.navigateContinue();
+      await amendApplicationDetailsPage.navigateContinue();
+      await amendApplicationDetailsPage.verifyDynamicExistingExpressPilotMessageIsVisible();
     }
   );
 });
