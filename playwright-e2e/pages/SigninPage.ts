18e42d7bbb2994a5cb3cf80e15e4dd51768fdb12
Paul Trelease <paul.trelease@justice.gov.uk>
2025-04-03 13:12:31 +0100

    Dfr 3684 playwright test amend form a for express (#2465)
    * amend application details event express case
    
    * Resilient logging
    
    ---------
    
    Co-authored-by: hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
    Co-authored-by: Ashley Wong <50691452+so99y@users.noreply.github.com>

--- playwright-e2e/pages/SigninPage.ts
+++ playwright-e2e/pages/SigninPage.ts
@@ -1,5 +1,4 @@
 import { type Page, type Locator, expect } from '@playwright/test';
-import config from '../config/config';
 
 export class SigninPage {
   
@@ -15,6 +14,7 @@ export class SigninPage {
     this.signinButtonLocator = page.getByRole('button', { name: 'Sign in' });
   }
 
+  // Generic login for any user.  No retries, less reliable.
   async login(email: string, password: string, expectedUrl: string) {   
     await expect(this.emailInputLocator).toBeVisible();
     await this.emailInputLocator.fill(email);
@@ -24,5 +24,37 @@ export class SigninPage {
     await expect(this.signinButtonLocator).toBeEnabled();
     await this.signinButtonLocator.click();
     await this.page.waitForURL(`${expectedUrl}/*`);
+  }
+
+  // Resilient login.  Requires the path that you expect a User to land on.
+  // For instance, Solictors and Caseworker land on pages with different paths.
+  // Faster timeout works for local running, but remains as safer Playwright default for AAT.
+  async loginWaitForPath(email: string, password: string, expectedUrl: string, requiredPath: string) {
+    const maxRetries = 10;
+
+    for (let attempt = 1; attempt <= maxRetries; attempt++) {
+      try {
+        await expect(this.emailInputLocator).toBeVisible();
+        await this.emailInputLocator.fill(email);
+
+        await expect(this.passwordInputLocator).toBeVisible();
+        await this.passwordInputLocator.fill(password);
+
+        await expect(this.signinButtonLocator).toBeVisible();
+        await expect(this.signinButtonLocator).toBeEnabled();
+        await this.signinButtonLocator.click();
+        
+        let timeoutAmount = 30000;
+        if ( expectedUrl === 'http://localhost:3000') {
+          timeoutAmount = 2000;
+        }
+
+        await this.page.waitForURL(`${expectedUrl}/${requiredPath}`, { timeout: timeoutAmount });
+        return;
+      } catch (err) {
+        if (attempt === maxRetries) throw err;
+        await this.page.waitForTimeout(200);
+      }
+    }
   }
 }
