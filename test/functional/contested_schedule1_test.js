b438f607b59ee914aeb882a6353014b5294d7393
tomfairclough1 <167980396+tomfairclough1@users.noreply.github.com>
2025-04-15 14:22:13 +0100

    DFR-3717: Contested Children's Act Creation by Solicitor (#2490)
    * DFR-3521: Created Contested Childrens Act test
    
    * DFR-3521: Update test and trying to fix radio button in genderOfChild function
    
    * Change to Nature of application for childrens
    
    * Added a way to dynamically select child gender radio button in create case
    
    * DFR-3521" Test completed and passing
    
    * DFR-3717: Removed old codecept test, added a force true to 'navigateContinue' due to EXUI flakiness
    
    * DFR-3717: Removed white spacing
    
    * DFR:3717: Changes from PR feedback extra/missing NL's
    
    * Fix NL's
    
    ---------
    
    Co-authored-by: gavinkeys <gdkeys6@gmail.com>
    Co-authored-by: Adam Lee <166380454+al-hmcts@users.noreply.github.com>

--- test/functional/contested_schedule1_test.js
+++ test/functional/contested_schedule1_test.js
@@ -16,25 +16,6 @@ const runningEnv = process.env.RUNNING_ENV;
 
 Feature('Contested Schedule 1 Tests');
 
-Scenario('Contested Schedule 1 Case Creation by Solicitor @nightly', async ({ I }) => {
-    await I.signInIdam(solicitorUserName, solicitorPassword);
-    await I.wait('2');
-    await I.createCase('FinancialRemedyContested', 'Form A Application');
-    await I.contestedSolicitorCreate(solRef, 'Schedule1');
-    await I.contestedApplicantDetails();
-    await I.childrenDetails();
-    await I.contestedRespondentDetails();
-    await I.contestedNatureOfApplicationForSchedule1();
-    await I.fastTrack();
-    await I.complexityList();
-    await I.applyingToCourt();
-    await I.mediationQuestion();
-    await I.miamCertification();
-    await I.contestedOtherDocuments();
-    await I.contestedCheckYourAnswers('Schedule1');
-    await I.waitForText('Form A Application', '60')
-}).retry(3);
-
 Scenario('Contested Schedule 1 Case Creation by Solicitor using API call @nightly', async ({ I }) => {
     //The json file used to create case is new case data - this can be used to create a case via solicitor, case type schedule 1.
     const caseId = await createCaseInCcd(solicitorUserName, solicitorPassword, './test/data/ccd-contested-schedule1-solicitor-create-case.json', 'FinancialRemedyContested', 'FR_solicitorCreate');
