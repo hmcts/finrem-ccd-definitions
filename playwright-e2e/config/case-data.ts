e55318f5fdc3b19657acd4b5bc54ca886cb90358
gavinkeys <gdkeys6@gmail.com>
2025-05-21 12:11:09 +0100

    Tidy up actions from PR comments

--- playwright-e2e/config/case-data.ts
+++ playwright-e2e/config/case-data.ts
@@ -1,12 +1,12 @@
 // Enums for Case Types and User Types
 export enum CaseType {
   Consented = "FinancialRemedyMVP2",
-  Contested = "FinancialRemedyContested",
+  Contested = "FinancialRemedyContested"
 }
 
 export enum UserType {
   Solicitor = "solicitor",
-  Caseworker = "caseworker",
+  Caseworker = "caseworker"
 }
 
 // Interface for Case Events
@@ -19,15 +19,15 @@ export interface CaseEvent {
 const CommonEvents: Record<string, CaseEvent> = {
   applicationPaymentSubmission: {
     listItem: "Case Submission",
-    ccdCallback: "FR_applicationPaymentSubmission",
+    ccdCallback: "FR_applicationPaymentSubmission"
   },
   hwfDecisionMade: {
     listItem: "HWF Application Accepted",
-    ccdCallback: "FR_HWFDecisionMade",
+    ccdCallback: "FR_HWFDecisionMade"
   },
   issueApplication: {
     listItem: "Issue Application",
-    ccdCallback: "FR_issueApplication",
+    ccdCallback: "FR_issueApplication"
   },
 };
 
@@ -35,79 +35,79 @@ const CommonEvents: Record<string, CaseEvent> = {
 export const ConsentedEvents: Record<string, CaseEvent> = {
   createCase: {
     listItem: "Consent Order Application",
-    ccdCallback: "FR_solicitorCreate",
+    ccdCallback: "FR_solicitorCreate"
   },
   approveApplication: {
     listItem: "Approve Application",
-    ccdCallback: "FR_approveApplication",
+    ccdCallback: "FR_approveApplication"
   },
   sendOrder: {
     listItem: "Send Order",
-    ccdCallback: "FR_sendOrderForApproved",
+    ccdCallback: "FR_sendOrderForApproved"
   },
   updateContactDetails: {
     listItem: "Update contact details",
-    ccdCallback: "FR_updateContactDetails",
+    ccdCallback: "FR_updateContactDetails"
   },
   createFlag: {
     listItem: "Create Flag",
-    ccdCallback: "createFlags",
+    ccdCallback: "createFlags"
   },
   manageFlags: {
     listItem: "Manage Flags",
-    ccdCallback: "manageFlags",
+    ccdCallback: "manageFlags"
   },
   // Common events
   applicationPaymentSubmission: CommonEvents.applicationPaymentSubmission,
   hwfDecisionMade: CommonEvents.hwfDecisionMade,
-  issueApplication: CommonEvents.issueApplication,
+  issueApplication: CommonEvents.issueApplication
 };
 
 // Contested Events
 export const ContestedEvents: Record<string, CaseEvent> = {
   createCase: {
     listItem: "Form A Application",
-    ccdCallback: "FR_solicitorCreate",
+    ccdCallback: "FR_solicitorCreate"
   },
   createPaperCase: {
     listItem: "Paper Case",
-    ccdCallback: "FR_newPaperCase",
+    ccdCallback: "FR_newPaperCase"
   },
   progressToListing: {
     listItem: "Progress to Listing",
-    ccdCallback: "FR_progressToSchedulingAndListing",
+    ccdCallback: "FR_progressToSchedulingAndListing"
   },
   listForHearing: {
     listItem: "List for Hearing",
-    ccdCallback: "FR_addSchedulingListingInfo",
+    ccdCallback: "FR_addSchedulingListingInfo"
   },
   amendFormAApplicationDetails: {
     listItem: "Amend Application Details",
-    ccdCallback: "FR_amendApplication",
+    ccdCallback: "FR_amendApplication"
   },
   manageExpressCase: {
     listItem: "Manage Express Case",
-    ccdCallback: "FR_manageExpressCase",
+    ccdCallback: "FR_manageExpressCase"
   },
   giveAllocationDirection: {
     listItem: "Give Allocation Directions",
-    ccdCallback: "FR_giveAllocationDirections",
+    ccdCallback: "FR_giveAllocationDirections"
   },
   amendPaperApplicationDetails: {
     listItem: "Amend Application Details",
-    ccdCallback: "FR_amendPaperApplication",
+    ccdCallback: "FR_amendPaperApplication"
   },
   manualPayment: {
     listItem: "Manual Payment",
-    ccdCallback: "FR_manualPayment",
+    ccdCallback: "FR_manualPayment"
   },
   allocateToJudge: {
     listItem: "Allocate to Judge",
-    ccdCallback: "FR_allocateToJudge",
+    ccdCallback: "FR_allocateToJudge"
   },
   listForInterimHearing: {
     listItem: "List for Interim Hearing",
-    ccdCallback: "FR_listForInterimHearing",
+    ccdCallback: "FR_listForInterimHearing"
   },
   uploadDraftOrders: {
       listItem: "Upload draft orders",
@@ -115,39 +115,39 @@ export const ContestedEvents: Record<string, CaseEvent> = {
   },
   generalApplicationDirections: {
     listItem: "General Application Directions",
-    ccdCallback: "FR_GeneralApplicationDirections",
+    ccdCallback: "FR_GeneralApplicationDirections"
   },
   createGeneralApplication: {
     listItem: "Create General Application",
-    ccdCallback: "createGeneralApplication",
+    ccdCallback: "createGeneralApplication"
   },
   generalApplicationReferToJudge: {
     listItem: "General Application Refer to Judge",
-    ccdCallback: "FR_generalApplicationReferToJudge",
+    ccdCallback: "FR_generalApplicationReferToJudge"
   },
   generalApplicationOutcome: {
     listItem: "General Application Outcome",
-    ccdCallback: "FR_GeneralApplicationOutcome",
+    ccdCallback: "FR_GeneralApplicationOutcome"
   },
   approveOrders: {
     listItem: "Approve Orders",
-    ccdCallback: "FR_approveOrders",
+    ccdCallback: "FR_approveOrders"
   },
   directionOrder: {
     listItem: "Direction Order",
-    ccdCallback: "FR_directionOrder",
+    ccdCallback: "FR_directionOrder"
   },
   // Common events
   applicationPaymentSubmission: CommonEvents.applicationPaymentSubmission,
   hwfDecisionMade: CommonEvents.hwfDecisionMade,
-  issueApplication: CommonEvents.issueApplication,
+  issueApplication: CommonEvents.issueApplication
 };
 
 // Payload Paths
 export const PayloadPath = {
   Consented: {
     base: "./playwright-e2e/data/case_data/consented/ccd-consented-case-creation.json",
-    createFlag: "./playwright-e2e/data/payload/consented/caseworker/create-flag.json",
+    createFlag: "./playwright-e2e/data/payload/consented/caseworker/create-flag.json"
   },
   Contested: {
     base: "./playwright-e2e/data/case_data/contested/ccd-contested-case-creation.json",
@@ -167,5 +167,5 @@ export const PayloadPath = {
     listForHearingFdaEgOne: "./playwright-e2e/data/payload/contested/caseworker/list-for-hearing/fda-example-one.json",
     judiciaryBasicApproveOrders: "./playwright-e2e/data/payload/contested/judiciary/most-basic-approve-orders.json",
     processOrderBasicTwoHearing: "./playwright-e2e/data/payload/contested/caseworker/process-order/basic-two-hearing.json"
-  },
+  }
 } as const;
