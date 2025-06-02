035a4dd7a0a73fbe8b7c3b08a2f541c089a238c4
James Turnbull <james.turnbull@justice.gov.uk>
2025-06-02 14:28:33 +0100

    Update E2E test code to use a valid PBA account number

--- playwright-e2e/functional/e2e.spec.ts
+++ playwright-e2e/functional/e2e.spec.ts
@@ -7,7 +7,7 @@ test(
   'Consented - Send Order Journey Test',
   { tag: [] },
   async (
-    { 
+    {
       loginPage,
       manageCaseDashboardPage,
       caseDetailsPage,
@@ -22,49 +22,49 @@ test(
   ) => {
 
     const caseId = await ConsentedCaseDataHelper.createConsentedCase();
-    
+
     // Login as Solicitor
     await manageCaseDashboardPage.visit();
     await loginPage.login(config.applicant_solicitor.email, config.applicant_solicitor.password, config.manageCaseBaseURL);
     await manageCaseDashboardPage.navigateToCase(caseId);
-  
-    // Application Payment Submission 
-    await caseDetailsPage.selectNextStep(ConsentedEvents.applicationPaymentSubmission); 
+
+    // Application Payment Submission
+    await caseDetailsPage.selectNextStep(ConsentedEvents.applicationPaymentSubmission);
     await solicitorAuthPage.enterSolicitorDetails('Bilbo Baggins', 'Bag End', 'Solicitor');
     await solicitorAuthPage.navigateContinue();
     await helpWithFeesPage.selectHelpWithFees(false);
     await helpWithFeesPage.navigateContinue();
-    await paymentPage.enterPaymentDetails('PBA0000539', 'Reference');
+    await paymentPage.enterPaymentDetails('PBA0089162', 'Reference');
     await paymentPage.navigateContinue();
     await orderSummaryPage.navigateContinue();
     await caseSubmissionPage.navigateContinue();
     await caseSubmissionPage.navigateSubmit();
     await caseSubmissionPage.returnToCaseDetails();
-  
+
     await caseDetailsPage.checkHasBeenUpdated(ConsentedEvents.applicationPaymentSubmission.listItem);
-  
+
     // Logout
     await manageCaseDashboardPage.signOut();
-  
+
     // Login as caseworker
     await loginPage.login(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL);
     await manageCaseDashboardPage.navigateToCase(caseId);
-  
+
     // Issue Application
-    await caseDetailsPage.selectNextStep(ConsentedEvents.issueApplication); 
+    await caseDetailsPage.selectNextStep(ConsentedEvents.issueApplication);
     await issueApplicationPage.navigateContinue();
     await issueApplicationPage.navigateSubmit();
     await caseDetailsPage.checkHasBeenUpdated(ConsentedEvents.issueApplication.listItem);
-  
+
     // Logout
     await manageCaseDashboardPage.signOut();
-  
+
     // Login in as judge
     await loginPage.login(config.judge.email, config.judge.password, config.manageCaseBaseURL);
     await manageCaseDashboardPage.navigateToCase(caseId);
-  
-    // Approve Application 
-    await caseDetailsPage.selectNextStep(ConsentedEvents.approveApplication); 
+
+    // Approve Application
+    await caseDetailsPage.selectNextStep(ConsentedEvents.approveApplication);
     await approveApplicationPage.selectIsSubjectTo(true);
     await approveApplicationPage.selectIsPensionProvider(false);
     await approveApplicationPage.selectJudge('District Judge');
