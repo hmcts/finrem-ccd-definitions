f340e0372c52bd64a0e95380a0c3a8ed52c6f0d8
Alwin-Joseph-Christopher <alwinjoseph.christopher@justice.gov.uk>
2025-06-09 11:34:13 +0100

    Fixed Intendation Error

--- playwright-e2e/functional/contested/FR_ManageHearings/manage_hearings.spec.ts
+++ playwright-e2e/functional/contested/FR_ManageHearings/manage_hearings.spec.ts
@@ -23,58 +23,58 @@ test.describe('Contested - Manage Hearings', () => {
     test(
         'Contested - Assert validations - Manage Hearings - Pre-Trial Review (PTR)', {
         tag: []},
-        async (
-            {loginPage, manageCaseDashboardPage, caseDetailsPage, manageHearingPage, checkYourAnswersPage}) => {
-        // Create and setup case
+        async ({loginPage, manageCaseDashboardPage, caseDetailsPage, manageHearingPage, checkYourAnswersPage}) => {
+
+            // Create and setup case
             const date = await DateHelper.getCurrentDate();
             const caseId = await ContestedCaseDataHelper.createAndProcessFormACaseUpToIssueApplication(false, date);
 
-        // Login as caseworker and navigate to case
-        await manageCaseDashboardPage.visit();
-        await loginPage.login(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL);
-        await manageCaseDashboardPage.navigateToCase(caseId);
-        console.info(`Navigated to case with ID: ${caseId}`);
+            // Login as caseworker and navigate to case
+            await manageCaseDashboardPage.visit();
+            await loginPage.login(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL);
+            await manageCaseDashboardPage.navigateToCase(caseId);
+            console.info(`Navigated to case with ID: ${caseId}`);
 
-        // Manage hearings
-        await caseDetailsPage.selectNextStep(ContestedEvents.manageHearings);
-        await manageHearingPage.navigateContinue();
-        await manageHearingPage.assertWhatWouldYouLikeToDoRequired();
+            // Manage hearings
+            await caseDetailsPage.selectNextStep(ContestedEvents.manageHearings);
+            await manageHearingPage.navigateContinue();
+            await manageHearingPage.assertWhatWouldYouLikeToDoRequired();
 
-        await manageHearingPage.selectAddANewHearing();
-        await manageHearingPage.navigateContinue();
+            await manageHearingPage.selectAddANewHearing();
+            await manageHearingPage.navigateContinue();
 
-        await manageHearingPage.assertErrorMessagesForAllMandatoryFields();
+            await manageHearingPage.assertErrorMessagesForAllMandatoryFields();
 
-        await manageHearingPage.uploadOtherDocuments("removeFile.pdf");
-        await manageHearingPage.removeUploadedDocument();
+            await manageHearingPage.uploadOtherDocuments("removeFile.pdf");
+            await manageHearingPage.removeUploadedDocument();
 
-        await manageHearingPage.enterHearingDate('asdf', 'asdf', 'asdf');
-        await manageHearingPage.assertHearingDateFormatError()
+            await manageHearingPage.enterHearingDate('asdf', 'asdf', 'asdf');
+            await manageHearingPage.assertHearingDateFormatError()
 
-        await manageHearingPage.addHearing({
-            type: "Pre-Trial Review (PTR)",
-            duration: '2 hours',
-            date: {},
-            time: '10:00 AM',
-            court: {zone: 'London', frc: 'London', courtName: 'CENTRAL FAMILY COURT'},
-            attendance: 'Remote - video call',
-            additionalInformation: 'Hearing details here',
-            uploadAnySupportingDocuments: true,
-            uploadFiles: ["final_hearing_file1.pdf", "final_hearing_file2.pdf"],
-            sendANoticeOfHearing: true
-        });
+            await manageHearingPage.addHearing({
+                type: "Pre-Trial Review (PTR)",
+                duration: '2 hours',
+                date: {},
+                time: '10:00 AM',
+                court: {zone: 'London', frc: 'London', courtName: 'CENTRAL FAMILY COURT'},
+                attendance: 'Remote - video call',
+                additionalInformation: 'Hearing details here',
+                uploadAnySupportingDocuments: true,
+                uploadFiles: ["final_hearing_file1.pdf", "final_hearing_file2.pdf"],
+                sendANoticeOfHearing: true
+            });
 
-        await manageHearingPage.navigateContinue();
-        await manageHearingPage.navigateIgnoreWarningAndContinue();
+            await manageHearingPage.navigateContinue();
+            await manageHearingPage.navigateIgnoreWarningAndContinue();
+    
+            const expectedTable = getManageHearingTableData({
+                typeOfHearing: "Pre-Trial Review (PTR)"
+            });
+            await checkYourAnswersPage.assertCheckYourAnswersPage(expectedTable);
 
-        const expectedTable = getManageHearingTableData({
-            typeOfHearing: "Pre-Trial Review (PTR)"
-        });
-        await checkYourAnswersPage.assertCheckYourAnswersPage(expectedTable);
+            await manageHearingPage.navigateSubmit();
 
-        await manageHearingPage.navigateSubmit();
-
-        await caseDetailsPage.checkHasBeenUpdated('Manage Hearings');
+            await caseDetailsPage.checkHasBeenUpdated('Manage Hearings');
 
     });
 
