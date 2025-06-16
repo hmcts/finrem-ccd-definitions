9cf3737a8a24766a740f2b4c33cbc1e73ee8ee39
bellalevene <bellalevene@gmail.com>
2025-06-16 11:42:02 +0100

    Updating page, test and table data

--- playwright-e2e/functional/consented/update_contact_details.spec.ts
+++ playwright-e2e/functional/consented/update_contact_details.spec.ts
@@ -4,6 +4,7 @@ import { ConsentedEvents } from '../../config/case-data';
 import { YesNoRadioEnum } from '../../pages/helpers/enums/RadioEnums';
 import { updateContactDetailsTabData } from '../../data/tab_content/consented/update_contact_details_caseworker_tabs';
 import { ConsentedCaseDataHelper } from '../helpers/Consented/ConsentedCaseDataHelper';
+import { consentedUpdateContactDetailsTableData } from '../../data/check_your_answer_content/update_contact_details/consentedUpdateContactDetailsTable';
 
 test(
     'Consented - Update contact details',
@@ -49,3 +50,42 @@ test(
       await caseDetailsPage.assertTabData(updateContactDetailsTabData);
     }
 );
+
+test(
+    'Consented - Update contact details - change in representation @test',
+    { tag: [] },
+    async (
+      {
+        loginPage,
+        manageCaseDashboardPage,
+        caseDetailsPage,
+        updateContactDetailsPage,
+        createCaseCheckYourAnswersPage,
+        checkYourAnswersPage
+      }) => {
+      // Create case and progress to HWF decision made
+      const caseId = await ConsentedCaseDataHelper.createConsentedCaseUpToHWFDecision();
+
+      // Login as caseworker
+      await manageCaseDashboardPage.visit();
+      await loginPage.login(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL);
+      await manageCaseDashboardPage.navigateToCase(caseId);
+
+      // Update contact details
+      await caseDetailsPage.selectNextStep(ConsentedEvents.updateContactDetails);
+      await updateContactDetailsPage.selectUpdateIncludesRepresentativeChange(true);
+      await updateContactDetailsPage.checkApplicantRepresented(true);
+      await updateContactDetailsPage.navigateContinue();
+      await updateContactDetailsPage.specifySolicitorName('Test Baggins'); 
+      await updateContactDetailsPage.navigateContinue();
+      await updateContactDetailsPage.specifyApplicantFirstName('Tester case');
+      await updateContactDetailsPage.navigateContinue();
+
+      //Continue about to submit and check your answers
+      await checkYourAnswersPage.assertCheckYourAnswersPage(consentedUpdateContactDetailsTableData);
+      await updateContactDetailsPage.navigateSubmit();
+      //check applicant tab info is correct
+      // check solicitor tab info is correct
+      }
+);
+
--- playwright-e2e/pages/events/update-contact-details/UpdateContactDetailsPage.ts
+++ playwright-e2e/pages/events/update-contact-details/UpdateContactDetailsPage.ts
@@ -9,6 +9,12 @@ export class UpdateContactDetailsPage extends BaseJourneyPage {
     private readonly applicantInRefugeRadio: Locator;
     private readonly respondentInRefugeAnswer: Locator;
     private readonly respondentInRefugeRadio: Locator;
+    private readonly applicantRadio: Locator;
+    private readonly respondentRadio: Locator;
+    private readonly checkSolicitorNameLabel: Locator;
+    private readonly solicitorNameDetails: Locator;
+    private readonly applicantFirstNameDetails: Locator;
+    private readonly checkApplicantFirstNameLabel: Locator;
 
     public constructor(page: Page) {
         super(page);
@@ -18,6 +24,13 @@ export class UpdateContactDetailsPage extends BaseJourneyPage {
 
         this.respondentInRefugeAnswer = page.getByRole('row', { name: 'Is the Respondent currently a resident in a refuge?' }).locator('span').nth(1);
         this.respondentInRefugeRadio = page.locator('#respondentInRefugeQuestion_radio');
+
+        this.applicantRadio = page.getByRole('radio', { name: 'Applicant' });
+        this.respondentRadio = page.getByLabel('Respondent');
+        this.checkSolicitorNameLabel = page.getByText('Solicitor’s name');
+        this.solicitorNameDetails = page.locator('#solicitorName');
+        this.checkApplicantFirstNameLabel = page.getByText('Current First and Middle names');
+        this.applicantFirstNameDetails = page.locator('#applicantFMName');
     }
 
     async selectUpdateIncludesRepresentativeChange(isUpdateIncludesRepresentativeChange: Boolean){
@@ -45,4 +58,15 @@ export class UpdateContactDetailsPage extends BaseJourneyPage {
         const optionToSelect = this.respondentInRefugeRadio.getByLabel(radioOption);
         await optionToSelect.check();
     }
+    async checkApplicantRepresented(isConfidential: boolean) {
+    await (isConfidential ? this.applicantRadio : this.respondentRadio).check(); 
+  }
+    async specifySolicitorName(text: string) {
+    await expect(this.checkSolicitorNameLabel).toBeVisible();
+    await this.solicitorNameDetails.fill(text);
+  }
+    async specifyApplicantFirstName(text: string) {
+    await expect(this.checkApplicantFirstNameLabel).toBeVisible();
+    await this.applicantFirstNameDetails.fill(text);
+  }
 }
