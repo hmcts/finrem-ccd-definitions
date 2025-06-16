856b62da56670d6f4617c084ff299f3faa3d7b8b
Alwin-Joseph-Christopher <alwinjoseph.christopher@justice.gov.uk>
2025-06-16 11:38:03 +0100

    fix review comment

--- playwright-e2e/test/smoke/smoke.spec.ts
+++ playwright-e2e/test/smoke/smoke.spec.ts
@@ -1,7 +1,5 @@
 import { test } from '../../fixtures/fixtures';
 import config from '../../config/config';
-// NOTE: When we remove codecept tests, bring utils and test data into the playwright directory
-import * as utils from '../../../test/helpers/utils';
 
 test(
     'Smoke Test - Check env running and can login',
