c37340425f42a76fefa5de8c4745c96366fcaeec
bellalevene <bellalevene@gmail.com>
2025-06-17 16:54:49 +0100

    Adding update contact details and change in representation flow

---
+++ playwright-e2e/resources/tab_content/consented/update_contact_details_represented.ts
@@ -0,0 +1,29 @@
+export const updateRepresentedContactDetailsTabData = [
+  {
+    tabName: 'Applicant',
+    tabContent: [
+      "APPLICANT DETAILS",
+      { tabItem: "Current First and Middle names", value: "Tester case" },
+      { tabItem: "Current Last Name", value: "Baggins" },
+      { tabItem: "Please state in which Financial Remedies Court Zone the applicant resides", value: "Midlands"},
+      { tabItem: "This should be the FRC local to the applicant", value: "Nottingham FRC" },
+      { tabItem: "Where is the Applicant’s Local Court?", value: "LEICESTER COUNTY COURT AND FAMILY COURT" },
+    "SOLICITOR DETAILS",
+      { tabItem: "Solicitor’s name", value: "Test Baggins" },
+      { tabItem: "Name of your firm", value: "FinRem-1-Org" },
+      { tabItem: "Your reference", value: "Y707HZM" },
+    ]
+  },
+  {
+    tabName: 'Respondent',
+    tabContent: [
+      "RESPONDENT DETAILS",
+      { tabItem: "Current First and Middle names", value: "Smeagol" },
+      { tabItem: "Current Last Name", value: "Gollum" },
+      { tabItem: "Is the respondent represented ?", value: "Yes" },
+      "RESPONDENT SOLICITOR’S DETAILS",
+      { tabItem: "Respondent Solicitor’s name", value: "Sauron" },
+      { tabItem: "Respondent solicitor’s firm", value: "Finrem Org 2" }
+    ]
+  }
+];
--- playwright-e2e/test/consented/update_contact_details.spec.ts
+++ playwright-e2e/test/consented/update_contact_details.spec.ts
@@ -4,6 +4,10 @@ import { ConsentedEvents } from '../../config/case-data';
 import { YesNoRadioEnum } from '../../pages/helpers/enums/RadioEnums';
 import { updateContactDetailsTabData } from '../../resources/tab_content/consented/update_contact_details_caseworker_tabs';
 import { ConsentedCaseFactory } from '../../data-utils/factory/consented/ConsentedCaseFactory';
+import { consentedUpdateContactDetailsTableData } from '../../resources/check_your_answer_content/update_contact_details/consentedUpdateContactDetailsTable';
+import { updateRepresentedContactDetailsTabData } from '../../resources/tab_content/consented/update_contact_details_represented';
+import { updateContactDetailsNotRepresentedTable } from '../../resources/check_your_answer_content/update_contact_details/updateContactdetailsNotRepresentedTable';
+import { updateNonRepresentedContactDetailsTabData  } from '../../resources/tab_content/consented/update_contact_details_not_represented';
 
 
 test(
@@ -48,5 +52,66 @@ test(
 
       // Assert tab data
       await caseDetailsPage.assertTabData(updateContactDetailsTabData);
+    }
+);
+
+
+test(
+    'Consented - Update contact details - change in representation @test',
+    { tag: [] },
+    async (
+      {
+        loginPage,
+        manageCaseDashboardPage,
+        caseDetailsPage,
+        updateContactDetailsPage,
+        checkYourAnswersPage
+      }) => {
+      // Create case and progress to HWF decision made
+      const caseId = await ConsentedCaseFactory.createConsentedCaseUpToHWFDecision();
+
+      // Login as caseworker
+      await manageCaseDashboardPage.visit();
+      await loginPage.login(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL);
+      await manageCaseDashboardPage.navigateToCase(caseId);
+
+      // Update contact details
+      await caseDetailsPage.selectNextStep(ConsentedEvents.updateContactDetails);
+      await updateContactDetailsPage.selectUpdateIncludesRepresentativeChange(true);
+      await updateContactDetailsPage.checkApplicantRepresented(true);
+      await updateContactDetailsPage.navigateContinue();
+      await updateContactDetailsPage.specifySolicitorName('Test Baggins'); 
+      await updateContactDetailsPage.navigateContinue();
+      await updateContactDetailsPage.specifyApplicantFirstName('Tester case');
+      await updateContactDetailsPage.navigateContinue();
+
+      //Continue about to submit and check your answers
+      await checkYourAnswersPage.assertCheckYourAnswersPage(consentedUpdateContactDetailsTableData);
+      await updateContactDetailsPage.navigateSubmit();
+      await caseDetailsPage.checkHasBeenUpdated(ConsentedEvents.updateContactDetails.listItem);
+
+      // Assert tab data
+      await caseDetailsPage.assertTabData(updateRepresentedContactDetailsTabData);
+
+      // Update contact details and make respondent not represented
+      await caseDetailsPage.selectNextStep(ConsentedEvents.updateContactDetails);
+      await updateContactDetailsPage.selectUpdateIncludesRepresentativeChange(true);
+      await updateContactDetailsPage.checkRespondentRepresented(false);
+      await updateContactDetailsPage.navigateContinue();
+      await updateContactDetailsPage.checkRepresentation(false);
+      await updateContactDetailsPage.navigateContinue();
+      await updateContactDetailsPage.enterAddress('NW2 7NE');
+      await updateContactDetailsPage.clickFindAddressButton();
+      await updateContactDetailsPage.selectAddress('10 Selsdon Road, London');
+      await updateContactDetailsPage.selectRespondentInRefuge(true);
+      await updateContactDetailsPage.navigateContinue();
+
+      //Continue about to submit and check your answers
+      await checkYourAnswersPage.assertCheckYourAnswersPage(updateContactDetailsNotRepresentedTable);
+      await updateContactDetailsPage.navigateSubmit();
+      await caseDetailsPage.checkHasBeenUpdated(ConsentedEvents.updateContactDetails.listItem);
+
+      // Assert tab data
+      await caseDetailsPage.assertTabData(updateNonRepresentedContactDetailsTabData);
     }
 );
