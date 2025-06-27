20e51433f4cd04e8bf06de6a364ec0534a9438f0
Alwin-Joseph-Christopher <alwinjoseph.christopher@justice.gov.uk>
2025-06-27 15:57:35 +0100

    Refactored tests to use login with retries for robustness
    # Conflicts:
    #	playwright-e2e/test/contested/FR_updateContactDetails/contested_update_contact_details.spec.ts

--- playwright-e2e/resources/check_your_answer_content/create_case/createCaseTable.ts
+++ playwright-e2e/resources/check_your_answer_content/create_case/createCaseTable.ts
@@ -255,4 +255,3 @@ export const  contestedCreateExpressFormAMatrimonyCaseDetailsTable: Table = {
         ...urgentCase
     ]
 }
-
--- playwright-e2e/test/consented/hwf_application_accepted.spec.ts
+++ playwright-e2e/test/consented/hwf_application_accepted.spec.ts
@@ -18,7 +18,7 @@ test(
 
     // Login as caseworker
     await manageCaseDashboardPage.visit();
-    await loginPage.login(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL);
+    await loginPage.loginWaitForPath(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL, config.loginPaths.worklist);
     await manageCaseDashboardPage.navigateToCase(caseId);
 
     // HWF Payment
