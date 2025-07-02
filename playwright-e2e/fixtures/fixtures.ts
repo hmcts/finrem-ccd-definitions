29a676cc75b7b0aee80b3eee4477e96d29cac9db
Alwin-Joseph-Christopher <alwinjoseph.christopher@justice.gov.uk>
2025-07-02 13:59:14 +0100

    Upload files will retry if rate limit error  popsup

--- playwright-e2e/fixtures/fixtures.ts
+++ playwright-e2e/fixtures/fixtures.ts
@@ -2,7 +2,7 @@ import { mergeTests, mergeExpects } from '@playwright/test';
 import { test as create } from './create-fixture';
 import { test as a11yTest, expect as a11yExpect } from './axe-fixture';
 import { CcdApi } from '../data-utils/api/CcdApi';
-import {CaseAssignmentApi} from "../data-utils/api/CaseAssignmentApi.ts";
+import {CaseAssignmentApi} from '../data-utils/api/CaseAssignmentApi.ts';
 
 export const test = mergeTests(create, a11yTest);
 export const expect = mergeExpects(a11yExpect);
