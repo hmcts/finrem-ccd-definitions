fc0686127b8b53ce754857a63834dec5e0e204e7
Alwin-Joseph-Christopher <alwinjoseph.christopher@justice.gov.uk>
2025-07-21 13:36:16 +0100

    Upload draft order suggested orders

--- playwright-e2e/resources/check_your_answer_content/upload_draft_order/uploadDraftOrderTable.ts
+++ playwright-e2e/resources/check_your_answer_content/upload_draft_order/uploadDraftOrderTable.ts
@@ -15,3 +15,16 @@ export const uploadDraftOrderTable: Table = {
         {cellItem: "Pension Sharing Annexes", value: "Pension Sharing Annexes 1Document BagginsFDA.pdf"}
     ]
 }
+
+export const uploadSuggestedDraftOrderTable: Table = {
+    tableName: 'Check your answers',
+    rows: [
+        {cellItem: "What kind of draft order do you need to upload?", value: "A suggested draft order prior to a listed hearing (this will just be placed on file for the hearing)"},
+        'Upload draft orders',
+        {cellItem: "Confirm the uploaded documents are for the case", value: "I confirm the uploaded documents are for the Baggins v Gollum case"},
+        {cellItem: "Who are you uploading this on behalf of?", value: "The applicant, Frodo Baggins" },
+        {cellItem: "What are you uploading?", value: "OrdersPension Sharing Annexes"},
+        {cellItem: "Upload draft order", value: "Upload draft order 1Document agreed-draft-order-document.docx"},
+        {cellItem: "Pension Sharing Annexes", value: "Pension Sharing Annexes 1Document BagginsFDA.pdf"}
+    ]
+}
--- playwright-e2e/test/contested/FR_UploadDraftOrder/upload_draft_order.spec.ts
+++ playwright-e2e/test/contested/FR_UploadDraftOrder/upload_draft_order.spec.ts
@@ -4,10 +4,10 @@ import config from "../../../config/config.ts";
 import {ContestedEvents} from "../../../config/case-data.ts";
 import {YesNoRadioEnum} from "../../../pages/helpers/enums/RadioEnums.ts";
 import {
-  uploadDraftOrderTable
+    uploadDraftOrderTable, uploadSuggestedDraftOrderTable
 } from "../../../resources/check_your_answer_content/upload_draft_order/uploadDraftOrderTable.ts";
 import {
-    approved_upload_draft_order_tabs,
+    approved_upload_draft_order_tabs, suggested_draft_order_case_document_tabs,
     upload_draft_order_tabs
 } from "../../../resources/tab_content/contested/upload_draft_order_tabs.ts";
 import {DateHelper} from "../../../data-utils/DateHelper.ts";
@@ -16,7 +16,7 @@ import {approveOrderTable} from "../../../resources/check_your_answer_content/ap
 
 test.describe('Contested - Upload Draft Order', () => {
   test(
-    'Form A case uploading a draft order',
+    'Form A case uploading a Agreed draft order',
     { tag: [] },
     async (
       {
@@ -47,7 +47,7 @@ test.describe('Contested - Upload Draft Order', () => {
       await uploadDraftOrdersPage.chooseThatYouAreUploadingOrders();
       await uploadDraftOrdersPage.chooseThatYouAreUploadingPensionSharingAnnexes();
       await uploadDraftOrdersPage.uploadDraftOrder(caseId);
-      await uploadDraftOrdersPage.uploadPensionSharingAnnexes(caseId);
+      await uploadDraftOrdersPage.uploadPensionSharingAnnexes();
       await uploadDraftOrdersPage.navigateContinue('submit');
 
       await checkYourAnswersPage.assertCheckYourAnswersPage(uploadDraftOrderTable);
@@ -55,6 +55,27 @@ test.describe('Contested - Upload Draft Order', () => {
       await uploadDraftOrdersPage.closeAndReturnToCaseDetails();
       await caseDetailsPage.checkHasBeenUpdated(ContestedEvents.uploadDraftOrders.listItem);
       await caseDetailsPage.assertTabData(upload_draft_order_tabs);
+
+        await caseDetailsPage.validateFileTree([
+            {
+                type: 'folder',
+                label: 'Post Hearing Draft Orders',
+                children : [
+                    {
+                        type: 'file',
+                        label: 'agreed-draft-order-document.docx',
+                        contentSnippets: [
+                            `Case Reference: ${caseId}`,
+                        ]
+                    },
+                    {
+                        type: 'file',
+                        label: 'BagginsFDA.pdf'
+                    }
+                ]
+            }
+        ])
+
       await manageCaseDashboardPage.signOut();
 
       // log in as judge to approve the orders
@@ -97,8 +118,89 @@ test.describe('Contested - Upload Draft Order', () => {
       await approvedOrderPage.closeAndReturnToCaseDetails();
       await caseDetailsPage.checkHasBeenUpdated(ContestedEvents.approveOrders.listItem);
       await caseDetailsPage.assertTabData(approved_upload_draft_order_tabs);
+      await manageCaseDashboardPage.signOut();
+
+      await manageCaseDashboardPage.visit();
+      await loginPage.loginWaitForPath(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL, config.loginPaths.worklist);
+      await manageCaseDashboardPage.navigateToCase(caseId);
+      await caseDetailsPage.assertTabData(approved_upload_draft_order_tabs);
+
     }
   );
+
+        test(
+            'Form A case uploading a Suggested draft order',
+            { tag: [] },
+            async (
+                {
+                    loginPage,
+                    manageCaseDashboardPage,
+                    caseDetailsPage,
+                    uploadDraftOrdersPage,
+                    checkYourAnswersPage,
+                    approvedOrderPage
+                }
+            ) => {
+                const caseId = await ContestedCaseFactory.progressToUploadDraftOrder({ isFormA: true });
+                let expectedUrl = ContestedEvents.uploadDraftOrders.ccdCallback;
+                await manageCaseDashboardPage.visit();
+                await loginPage.loginWaitForPath(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL, config.loginPaths.worklist);
+                await manageCaseDashboardPage.navigateToCase(caseId);
+
+                await caseDetailsPage.selectNextStep(ContestedEvents.uploadDraftOrders);
+
+                await uploadDraftOrdersPage.chooseASuggestedDraftOrderPriorToAListedHearing();
+                await uploadDraftOrdersPage.navigateContinue(expectedUrl, 2);
+                await uploadDraftOrdersPage.assertMandatoryFields(false);
+
+                await uploadDraftOrdersPage.confirmTheUploadedDocsAreForTheCase();
+                await uploadDraftOrdersPage.chooseUploadOnBehalfOfApplicant();
+                await uploadDraftOrdersPage.chooseThatYouAreUploadingOrders();
+                await uploadDraftOrdersPage.chooseThatYouAreUploadingPensionSharingAnnexes();
+                await uploadDraftOrdersPage.uploadDraftOrder(caseId);
+                await uploadDraftOrdersPage.uploadPensionSharingAnnexes();
+                await uploadDraftOrdersPage.navigateContinue('submit');
+
+                await checkYourAnswersPage.assertCheckYourAnswersPage(uploadSuggestedDraftOrderTable);
+                await uploadDraftOrdersPage.navigateSubmit();
+                await uploadDraftOrdersPage.closeAndReturnToCaseDetails();
+                await caseDetailsPage.checkHasBeenUpdated(ContestedEvents.uploadDraftOrders.listItem);
+                await caseDetailsPage.assertTabData(suggested_draft_order_case_document_tabs);
+                await caseDetailsPage.validateFileTree([
+                    {
+                        type: 'folder',
+                        label: 'Hearing Documents',
+                        children : [
+                            {
+                                type: 'folder',
+                                label: 'Applicant',
+                                children: [
+                                    {
+                                        type: 'folder',
+                                        label: 'Pre Hearing Draft Order',
+                                        children: [
+                                            {
+                                                type: 'file',
+                                                label: 'agreed-draft-order-document.docx',
+                                                contentSnippets: [
+                                                    `Case Reference: ${caseId}`,
+                                                ]
+                                            },
+                                            {
+                                                type: 'file',
+                                                label: 'BagginsFDA.pdf'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                ])
+
+                await manageCaseDashboardPage.signOut();
+            }
+        );
 }
 
 );
