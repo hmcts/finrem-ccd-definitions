59b02b01e47e0d92533b4c1e13deb5275a88d4c6
bellalevene <bellalevene@gmail.com>
2025-07-30 11:58:41 +0100

    adding tag

--- playwright-e2e/test/contested/FR_sendOrder/send_order.spec.ts
+++ playwright-e2e/test/contested/FR_sendOrder/send_order.spec.ts
@@ -58,7 +58,7 @@ import { contestedSendOrderTabData } from '../../../resources/tab_content/contes
 
 test.describe('Contested - Approved and Send Order', () => {
   test(
-    'Form A case up to process order @test',
+    'Form A case up to process order @testing',
     { tag: ['@process-order'] },
     async (
       {
