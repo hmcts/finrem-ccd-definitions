a1718a8db0a0419cb470f708d5340ec8ee2d2c9a
BellaLevene <67078406+BellaLevene@users.noreply.github.com>
2025-07-31 11:55:59 +0100

    DFR-3961 - PW Test - Contested Case Creation For Ready For Hearing (#2730)
    * Ready for Hearing test
    
    * adding change to send order page

--- playwright-e2e/fixtures/create-fixture.ts
+++ playwright-e2e/fixtures/create-fixture.ts
@@ -69,6 +69,7 @@ import { ReferToJudgeApplicationPage } from '../pages/events/refer-to-judge-appl
 import { GeneralApplicationOutcomePage } from '../pages/events/general-application-outcome/GeneralApplicationOutcomePage.ts';
 import {ApproveOrderPage} from '../pages/events/approve-order/ApproveOrderPage.ts';
 import { ProcessOrderPage } from '../pages/events/process-order/ProcessOrderPage.ts';
+import { SubmitUploadedCaseFilesPage } from '../pages/events/submit-uploaded-case-files/SubmitUploadedCaseFilesPage.ts';
 import { UnprocessedApprovedOrdersPage } from '../pages/events/process-order/UnprocessedApprovedOrdersPage.ts';
 import { ProcessOrderHearingDetailsPage } from '../pages/events/process-order/ProcessOrderHearingDetailsPage.ts';
 import {ConsentApplicationApprovedPage} from '../pages/events/consent-application/ConsentApplicationApprovedPage.ts';
@@ -147,6 +148,7 @@ type CreateFixtures = {
   generalApplicationOutcomePage: GeneralApplicationOutcomePage;
   approvedOrderPage: ApproveOrderPage;
   processOrderPage: ProcessOrderPage;
+  submitUploadedCaseFilesPage : SubmitUploadedCaseFilesPage;
   consentApplicationApprovePage: ConsentApplicationApprovedPage;
   consentOrderNotApprovedPage: ConsentOrderNotApprovedPage;
 };
@@ -358,6 +360,9 @@ export const test = base.extend<CreateFixtures>({
   },
   processOrderPage: async ({ page }, use) => {
     await use(new ProcessOrderPage(page, commonActionsHelper));
+  },
+  submitUploadedCaseFilesPage: async ({ page }, use) => {
+    await use(new SubmitUploadedCaseFilesPage(page));
   },
   consentApplicationApprovePage: async ({ page }, use) => {
     await use(new ConsentApplicationApprovedPage(page, commonActionsHelper));
