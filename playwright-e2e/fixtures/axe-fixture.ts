13a4035356c625e4b829e501b1249e7b7f00c09a
Alwin-Joseph-Christopher <alwinjoseph.christopher@justice.gov.uk>
2025-08-05 16:48:01 +0100

    removed references to old axe fixtures

--- playwright-e2e/fixtures/axe-fixture.ts
+++ playwright-e2e/fixtures/axe-fixture.ts
@@ -3,25 +3,10 @@ import AxeBuilder from '@axe-core/playwright';
 import {AxeUtils} from './utils/axe-utils.ts';
 
 type AxeFixture = {
-  makeAxeBuilder: () => AxeBuilder;
   axeUtils: AxeUtils;
 };
 
 export const test = base.extend<AxeFixture>({
-  makeAxeBuilder: async ({ page }, use) => {
-    const makeAxeBuilder = () => {
-      return new AxeBuilder({ page }).withTags([
-        'wcag2a',
-        'wcag2aa',
-        'wcag21a',
-        'wcag21aa',
-        'wcag22a',
-        'wcag22aa'
-      ]);
-    };
-
-    await use(makeAxeBuilder);
-  },
   axeUtils: async ({ page }, use) => {
     await use(new AxeUtils(page));
   }
