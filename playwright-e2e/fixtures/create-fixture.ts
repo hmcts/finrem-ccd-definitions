8001795b4a8054bc786dcdb99e03b2eae76b979c
James Turnbull <81160745+jthmcts@users.noreply.github.com>
2025-09-25 15:38:21 +0100

    DFR-4105: Add judge Upload approved order E2E tests (#2833)
    * Empty commit
    
    * Enable secure doc store for testing
    
    * Create test for Judge upload approved order
    
    * updated test
    
    * Updated test
    
    * Ensure finrem-cos pointing to preview version of cdam
    
    * Added tab assertion and accessibility audit
    
    * Revert secure doc store updates for preview env
    
    ---------
    
    Co-authored-by: tomfairclough1 <thomas.fairclough@justice.gov.uk>

--- playwright-e2e/fixtures/create-fixture.ts
+++ playwright-e2e/fixtures/create-fixture.ts
@@ -79,6 +79,7 @@ import { GeneralApplicationDirectionsMHPage } from '../pages/events/general-appl
 import { UploadApprovedOrderPage } from '../pages/events/upload-approved-order/UploadApprovedOrderPage';
 import { UploadApprovedOrderMHPage } from '../pages/events/upload-approved-order/UploadApprovedOrderMHPage.ts';
 import {NoticeOfChangePage} from '../pages/NoticeOfChangePage.ts';
+import { JudgeUploadApprovedOrderPage } from '../pages/events/judge-upload-approved-order/JudgeUploadApprovedOrderPage.ts';
 
 const commonActionsHelper = new CommonActionsHelper();
 const solicitorDetailsHelper = new SolicitorDetailsHelper();
@@ -161,6 +162,7 @@ type CreateFixtures = {
   uploadApprovedOrderPage: UploadApprovedOrderPage;
   uploadApprovedOrderMHPage: UploadApprovedOrderMHPage;
   noticeOfChangePage: NoticeOfChangePage;
+  judgeUploadApprovedOrderPage: JudgeUploadApprovedOrderPage;
 };
 
 export const test = base.extend<CreateFixtures>({
@@ -394,5 +396,8 @@ export const test = base.extend<CreateFixtures>({
   },
   noticeOfChangePage: async ({ page }, use) => {
     await use(new NoticeOfChangePage(page));
+  },
+  judgeUploadApprovedOrderPage: async ({ page }, use) => {
+    await use(new JudgeUploadApprovedOrderPage(page, commonActionsHelper));
   }
 });
