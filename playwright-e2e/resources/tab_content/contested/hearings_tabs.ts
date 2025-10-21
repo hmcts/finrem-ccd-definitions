cef3a4d6409d782bb975babc84deccc5fc52c45c
hamzahtahirhmcts <162336174+hamzahtahirhmcts@users.noreply.github.com>
2025-10-21 11:30:16 +0100

    DFR-4107 Add Collection Table View to Hearings tab for all roles (#2816)
    * Add Collection Table View to Hearings tab for all roles
    
    * assert new style tab for Hearings
    
    * Remove uneeded import
    
    * Changed court data
    
    ---------
    
    Co-authored-by: tomfairclough1 <167980396+tomfairclough1@users.noreply.github.com>
    Co-authored-by: tomfairclough1 <thomas.fairclough@justice.gov.uk>

--- playwright-e2e/resources/tab_content/contested/hearings_tabs.ts
+++ playwright-e2e/resources/tab_content/contested/hearings_tabs.ts
@@ -1,19 +1,23 @@
 import { DateHelper } from "../../../data-utils/DateHelper";
 import { Tab } from "../../../pages/components/tab";
 
-export const processOrderHearingTabData =[{ 
-  tabName: "Hearings",
-  tabContent: [
-    "Hearings",
-    { tabItem: "Type of Hearing", value: "First Directions Appointment (FDA)" },
-    { tabItem: "Court", value: "Bromley County Court And Family Court" },
-    { tabItem: "Hearing Attendance", value: "In Person" },
-    { tabItem: "Hearing Date", value: "01 Jan 2024 10:00" },
-    { tabItem: "Hearing Time Estimate", value: "30" }, 
-    { tabItem: "Who has received this notice", value: "Applicant - Frodo Baggins, Respondent - Smeagol Gollum" },
-    { tabItem: "Additional information about the hearing", value: "This is a test hearing" },
-  ]
-}];
+export const processOrderHearingTabData = [
+  {
+    tabName: 'Hearings',
+    tabContent: buildHearingsTabContent([
+      {
+        typeOfHearing: "First Directions Appointment (FDA)",
+        hearingDate: "01 Jan 2024 10:00",
+        whoHasReceivedThisNotice: "Applicant - Frodo Baggins, Respondent - Smeagol Gollum",
+        court: "Bromley County Court And Family Court",
+        hearingAttendance: "In Person",
+        hearingTimeEstimate: "30",
+        additionalInformation: "This is a test hearing",
+        hearingDocuments: 'HearingNotice.pdf\nForm-G.pdf\nPfdNcdrComplianceLetter.pdf\nPfdNcdrCoverLetter.pdf\nOutOfFamilyCourtResolution.pdf\nForm-C.pdf'
+      }
+    ])
+  }
+];
 
 export function newUploadApprovedOrderMHTabDataOnHearing1() : Tab[] {
   return [
@@ -35,4 +39,34 @@ export function newUploadApprovedOrderMHTabDataOnHearing1() : Tab[] {
       ]
     }
   ];
+}
+
+export function buildHearingsTabContent(hearings: Array<{
+  typeOfHearing: string,
+  hearingDate: string,
+  whoHasReceivedThisNotice: string,
+  court: string,
+  hearingAttendance?: string,
+  hearingTimeEstimate: string,
+  additionalInformation: string,
+  hearingDocuments?: string
+}>) {
+  const tabContent: any[] = [];
+  if (hearings.length > 0) {
+    tabContent.push("Hearing");
+  }
+  hearings.forEach(hearing => {
+    tabContent.push({ tabItem: "Type of Hearing", value: "Hearing Date | Who has received this notice", exact: false });
+    tabContent.push({
+      tabItem: hearing.typeOfHearing,
+      value: `${hearing.hearingDate} | ${hearing.whoHasReceivedThisNotice}`,
+      clickable: true
+    });
+    tabContent.push({ tabItem: "Court", value: hearing.court });
+    tabContent.push({ tabItem: "Hearing Attendance", value: hearing.hearingAttendance });
+    tabContent.push({ tabItem: "Hearing Time Estimate", value: hearing.hearingTimeEstimate });
+    tabContent.push({ tabItem: "Additional information about the hearing", value: hearing.additionalInformation });
+    tabContent.push({ tabItem: "Hearing Documents", value: hearing.hearingDocuments });
+  });
+  return tabContent;
 }
