31181a337bd424865cb5f6baa9f3529cc44f3472
Zakaria <Zakaria.Markhi@justice.gov.uk>
2026-01-19 11:51:14 +0000

    DFR-4324 CT/CS: Consume common F&P Payment History tab component (#3011)
    * Add Payment History tab for contested and consented
    
    * Add Payment History tab configuration for contested in prod files
    
    * Fix failing tests
    
    * add consented payment history tab to prod
    
    * add consented payment history tab to prod
    
    * Playwright DFR-4324
    
    * Playwright workj
    
    * Playwright test
    
    * Update PW test
    
    * add authorisation for hwfFeeReferenceNumber for consented
    
    * Fix typo in caseID
    
    * Playwright test for HWF flow and helper
    
    * Add space in AuthorisationCaseField
    
    * Add space in AuthorisationCaseField
    
    ---------
    
    Co-authored-by: tomfairclough1 <thomas.fairclough@justice.gov.uk>
    Co-authored-by: Tom-HMCTS <167980396+Tom-HMCTS@users.noreply.github.com>
    Co-authored-by: so99y <50691452+so99y@users.noreply.github.com>

--- playwright-e2e/resources/tab_content/payment_details_tabs.ts
+++ playwright-e2e/resources/tab_content/payment_details_tabs.ts
@@ -1,10 +1,42 @@
+import { DateHelper } from '../../data-utils/DateHelper.ts';
 import {YesNoRadioEnum} from '../../pages/helpers/enums/RadioEnums.ts';
 
-export const paymentDetailsTabData  = (helpWithFees: YesNoRadioEnum, pbaNumber: string, reference: string) => {return [{
-  tabName: 'Payment details',
+export const paymentDetailsTabData = (
+  feeCode: string,
+  feeType: string,
+  amount: string,
+  hwfCode?: string
+) => {return [{
+  tabName: 'Payment History',
   tabContent: [
-    { tabItem: 'Has the applicant applied for help with fees online?', value: helpWithFees},
-    { tabItem: 'Enter your account number', value: pbaNumber },
-    { tabItem: 'Enter your reference', value: reference }
+    'Order Summary',
+    feeCode,
+    feeType,
+    amount
   ]
-}];};
+}];
+};
+
+export function paymentDetailsReviewData(
+  amount: string,
+  helpWithFees: YesNoRadioEnum,
+  feeCode: string,  
+  pbaNumber: string,
+  reference: string,
+  currentDateTimeFull: string
+) {
+  return [
+    'Payment details',
+    { tabItem: 'Payment amount', value: amount },
+    {
+      tabItem: 'Payment method',
+      value: helpWithFees === YesNoRadioEnum.NO ? 'payment by account' : 'HWF'
+    },
+    { tabItem: 'PBA account name', value: feeCode === 'FEE0228' ? 'FinRem-1-Org' : 'Bag End' },
+    { tabItem: 'PBA number', value: pbaNumber },
+    { tabItem: 'Customer internal reference', value: reference },
+    'Payment status history',
+    { tabItem: 'Status', value: 'Date and time' },
+    { tabItem: 'Success', value: currentDateTimeFull, exact: false }
+  ];
+}
