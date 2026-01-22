608ef6657096791ec1e227479d5b9b8f409b7a2f
Abdirahman Hussein <Abdirahman.Hussein@HMCTS.NET>
2026-01-22 16:04:21 +0000

    Familyfr main (#3054)
    * Update dependency chai to v6.2.2 (#3024)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency org.junit:junit-bom to v5.14.2 (#3025)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * DFR-4391: Playwright (#3028)
    
    * Empty commit
    
    * Playwright test added for DFR-4391
    
    * Fix comma and remove comment in error
    
    * Spacing and NL fixes
    
    ---------
    
    Co-authored-by: Tom-HMCTS <Tom-HMCTS@users.noreply.github.com>
    Co-authored-by: tomfairclough1 <thomas.fairclough@justice.gov.uk>
    Co-authored-by: so99y <50691452+so99y@users.noreply.github.com>
    
    * suppress otplib (#3031)
    
    Co-authored-by: Ashley Wong <im@so99y.com>
    
    * Update dependency @hmcts/properties-volume to v1.3.0 (#3029)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency @eslint/plugin-kit to v0.5.1 (#3033)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Edge Accessibility fix Nightly tests (#3036)
    
    * Empty commit
    
    * move accesbility toggle to Edge block
    
    ---------
    
    Co-authored-by: ptrelease <ptrelease@users.noreply.github.com>
    
    * Update dependency @types/lodash to v4.17.23 (#3034)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Accessibility: exclude document tree from axe aria-required-parent checks (#3039)
    
    * Empty commit
    
    * Exclude document-tree-container accessibility checks due to EXUI-4025
    
    ---------
    
    Co-authored-by: ptrelease <ptrelease@users.noreply.github.com>
    
    * Update dependency @types/node to v22.19.6 (#3038)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * Update dependency @hmcts/playwright-common to v1.1.0 (#3040)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * DFR-4324 CT/CS: Consume common F&P Payment History tab component (#3011)
    
    * Add Payment History tab for contested and consented
    
    * Add Payment History tab configuration for contested in prod files
    
    * Fix failing tests
    
    * add consented payment history tab to prod
    
    * add consented payment history tab to prod
    
    * Playwright DFR-4324
    
    * Playwright workj
    
    * Playwright test
    
    * Update PW test
    
    * add authorisation for hwfFeeReferenceNumber for consented
    
    * Fix typo in caseID
    
    * Playwright test for HWF flow and helper
    
    * Add space in AuthorisationCaseField
    
    * Add space in AuthorisationCaseField
    
    ---------
    
    Co-authored-by: tomfairclough1 <thomas.fairclough@justice.gov.uk>
    Co-authored-by: Tom-HMCTS <167980396+Tom-HMCTS@users.noreply.github.com>
    Co-authored-by: so99y <50691452+so99y@users.noreply.github.com>
    
    * Fix for PBA tests in Nightly (#3047)
    
    * Empty commit
    
    * fix locator and added return method to caputre data and time for navigateSubmit
    
    * Update PaymentSubmissionHelper.ts
    
    remove empty space
    
    ---------
    
    Co-authored-by: ptrelease <ptrelease@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Tom-HMCTS <Tom-HMCTS@users.noreply.github.com>
    Co-authored-by: tomfairclough1 <thomas.fairclough@justice.gov.uk>
    Co-authored-by: so99y <50691452+so99y@users.noreply.github.com>
    Co-authored-by: Ashley Wong <im@so99y.com>
    Co-authored-by: Tom-HMCTS <167980396+Tom-HMCTS@users.noreply.github.com>
    Co-authored-by: ptrelease <ptrelease@users.noreply.github.com>
    Co-authored-by: Zakaria <Zakaria.Markhi@justice.gov.uk>

--- playwright-e2e/test/consented/application_payment_submission.spec.ts
+++ playwright-e2e/test/consented/application_payment_submission.spec.ts
@@ -2,26 +2,24 @@ import { test } from '../../fixtures/fixtures';
 import config from '../../config/config';
 import { ConsentedEvents } from '../../config/case-data';
 import { ConsentedCaseFactory } from '../../data-utils/factory/consented/ConsentedCaseFactory';
-import {YesNoRadioEnum} from '../../pages/helpers/enums/RadioEnums.ts';
-import {applicationCaseSubmission} from '../../pages/helpers/PaymentSubmissionHelper.ts';
-import {envTestData} from '../../data-utils/test_data/EnvTestDataConfig.ts';
+import { YesNoRadioEnum } from '../../pages/helpers/enums/RadioEnums.ts';
+import { applicationCaseSubmission, applicationCaseSubmissionHWF } from '../../pages/helpers/PaymentSubmissionHelper.ts';
+import { envTestData } from '../../data-utils/test_data/EnvTestDataConfig.ts';
 
 test(
-  'Consented - Application Payment Submission',
-  { tag: ['@chromium'] },
-  async (
-    {
-      loginPage,
-      manageCaseDashboardPage,
-      caseDetailsPage,
-      solicitorAuthPage,
-      helpWithFeesPage,
-      paymentPage,
-      orderSummaryPage,
-      caseSubmissionPage,
-      checkYourAnswersPage
-    }
-  ) => {
+  'Consented - Case Submission - PBA Payment',
+  { tag: ['@chromium', '@payment'] },
+  async ({
+    loginPage,
+    manageCaseDashboardPage,
+    caseDetailsPage,
+    solicitorAuthPage,
+    helpWithFeesPage,
+    paymentPage,
+    orderSummaryPage,
+    caseSubmissionPage,
+    checkYourAnswersPage
+  }) => {
     // Create case
     const caseId = await ConsentedCaseFactory.createConsentedCase();
 
@@ -29,6 +27,9 @@ test(
     const pbaNumber = envTestData.PBA_NUMBER;
     const reference = 'Reference';
     const hasHelpWithFees = YesNoRadioEnum.NO;
+    const amount = '£60.00';
+    const feeCode = 'FEE0228';
+    const feeType = 'Application (without notice)';
 
     // Login as caseworker
     await manageCaseDashboardPage.visit();
@@ -47,7 +48,61 @@ test(
         hasHelpWithFees: hasHelpWithFees,
         pbaNumber: pbaNumber,
         reference: reference,
-        amount: '£60.00'
+        amount: amount,
+        feeCode: feeCode,
+        feeType: feeType
+      },
+      [
+        ['FEE0228', 'Application (without notice)', '£60.00'],
+        ['', 'Total', '£60.00']
+      ]
+    );
+  }
+);
+
+test(
+  'Consented - Case Submission - HWF Payment',
+  { tag: ['@payment', '@hwf'] },
+  async ({
+    loginPage,
+    manageCaseDashboardPage,
+    caseDetailsPage,
+    solicitorAuthPage,
+    helpWithFeesPage,
+    orderSummaryPage,
+    caseSubmissionPage,
+    checkYourAnswersPage
+  }) => {
+    // Create case
+    const caseId = await ConsentedCaseFactory.createConsentedCase();
+
+    // Define common test data
+    const reference = 'Reference';
+    const hasHelpWithFees = YesNoRadioEnum.YES;
+    const hwfCode = 'HWF123123';
+    const amount = '£60.00';
+    const feeCode = 'FEE0228';
+    const feeType = 'Application (without notice)';
+
+    // Login as caseworker
+    await manageCaseDashboardPage.visit();
+    await loginPage.loginWaitForPath(config.applicant_solicitor.email, config.applicant_solicitor.password, config.manageCaseBaseURL, config.loginPaths.cases);
+    await manageCaseDashboardPage.navigateToCase(caseId);
+    await applicationCaseSubmissionHWF(
+      caseDetailsPage,
+      solicitorAuthPage,
+      helpWithFeesPage,
+      orderSummaryPage,
+      caseSubmissionPage,
+      checkYourAnswersPage,
+      {
+        caseEvent: ConsentedEvents.applicationPaymentSubmission,
+        hasHelpWithFees: hasHelpWithFees,
+        hwfCode: hwfCode,
+        reference: reference,
+        amount: amount,
+        feeCode: feeCode,
+        feeType: feeType
       },
       [
         ['FEE0228', 'Application (without notice)', '£60.00'],
