628d5339394c77f0351f22775ba7628f1abc8b44
tomfairclough1 <thomas.fairclough@justice.gov.uk>
2026-02-09 16:02:02 +0000

    Added PW test to cover changing address scenario in  Representation change Yes flow without changing representation

--- playwright-e2e/test/contested/FR_updateContactDetails/update_contact_details.spec.ts
+++ playwright-e2e/test/contested/FR_updateContactDetails/update_contact_details.spec.ts
@@ -2,9 +2,9 @@ import { test } from '../../../fixtures/fixtures.ts';
 import config from '../../../config/config.ts';
 import { ContestedEvents } from '../../../config/case-data.ts';
 import { YesNoRadioEnum } from '../../../pages/helpers/enums/RadioEnums.ts';
-import { contestedUpdateContactDetailsTableData } from '../../../resources/check_your_answer_content/update_contact_details/updateContactDetailsTable.ts';
+import { contestedUpdateContactDetailsRespondentRepresentedAddressChangeTable, contestedUpdateContactDetailsTableData } from '../../../resources/check_your_answer_content/update_contact_details/updateContactDetailsTable.ts';
 import {ContestedCaseFactory} from '../../../data-utils/factory/contested/ContestedCaseFactory.ts';
-import { updateContestedApplicantRepresentedContactDetailsTabData } from '../../../resources/tab_content/contested/update_contact_details_represented.ts';
+import { contestedUpdateContactDetailsRespondentRepresentedAddressChangeTabData, updateContestedApplicantRepresentedContactDetailsTabData } from '../../../resources/tab_content/contested/update_contact_details_represented.ts';
 import { contestedUpdateContactDetailsTabData } from '../../../resources/tab_content/contested/contested_update_contact_details_caseworker_tabs.ts';
 import { contestedApplicantUpdateContactDetailsTableData } from '../../../resources/check_your_answer_content/update_contact_details/updateContactDetailsTable.ts';
 import { contestedUpdateContactDetailsRespondentNotRepresentedTable } from '../../../resources/check_your_answer_content/update_contact_details/updateContactDetailsTable.ts';
@@ -115,5 +115,47 @@ test(
 
     // Assert tab data
     await caseDetailsPage.assertTabData(updateContestedRespondentNonRepresentedContactDetailsTabData);
+  }
+);
+
+test(
+  'Contested - Update contact details - change in representation - without changing/removing representation details',
+  { tag: [] },
+  async (
+    {
+      loginPage,
+      manageCaseDashboardPage,
+      caseDetailsPage,
+      updateContactDetailsPage,
+      checkYourAnswersPage
+    }) => {
+    // Create case and progress to HWF decision made
+    const caseId = await ContestedCaseFactory.createContestedCaseUpToHWFDecision();
+
+    // Login as caseworker
+    await manageCaseDashboardPage.visit();
+    await loginPage.loginWaitForPath(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL, config.loginPaths.worklist);
+    await manageCaseDashboardPage.navigateToCase(caseId);
+    
+    // Update contact details and change respondent address without changing/removing representation details
+    await caseDetailsPage.selectNextStep(ContestedEvents.updateContactDetails);
+    await updateContactDetailsPage.selectUpdateIncludesRepresentativeChange(true);
+    await updateContactDetailsPage.checkRespondentRepresented(false);
+    await updateContactDetailsPage.navigateContinue();
+    await updateContactDetailsPage.checkRepresentation(true);
+    await updateContactDetailsPage.enterAddress('NW2 7NE');
+    await updateContactDetailsPage.clickFindAddressButton();
+    await updateContactDetailsPage.selectAddress('10 Selsdon Road, London');
+    await updateContactDetailsPage.navigateContinue();
+    await updateContactDetailsPage.selectRespondentInRefuge(true);
+    await updateContactDetailsPage.navigateContinue();
+
+    //Continue about to submit and check your answers
+    await checkYourAnswersPage.assertCheckYourAnswersPage(contestedUpdateContactDetailsRespondentRepresentedAddressChangeTable); 
+    await updateContactDetailsPage.navigateSubmit();
+    await caseDetailsPage.checkHasBeenUpdated(ContestedEvents.updateContactDetails.listItem);
+
+    // Assert tab data
+    await caseDetailsPage.assertTabData(contestedUpdateContactDetailsRespondentRepresentedAddressChangeTabData);
   }
 );
