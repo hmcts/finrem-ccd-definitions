28d39987e02e945c1a1d7e45f2b95308c65877cb
Austen Stevens <austen.stevens@justice.gov.uk>
2026-04-20 10:52:25 +0100

    consent test fixes (#3232)
    Co-authored-by: so99y <50691452+so99y@users.noreply.github.com>

--- playwright-e2e/resources/tab_content/consented/update_contact_details_not_represented.ts
+++ playwright-e2e/resources/tab_content/consented/update_contact_details_not_represented.ts
@@ -38,9 +38,9 @@ export const updateRespondentNonRepresentedContactDetailsSolChangeTabData = [
     tabName: 'Applicant',
     tabContent: [
       'APPLICANT DETAILS',
-      { tabItem: 'Current First and Middle names', value: 'Tester case' },
+      { tabItem: 'Current First and Middle names', value: 'Frodo' },
       { tabItem: 'Current Last Name', value: 'Baggins' },
-      { tabItem: 'Please state in which Financial Remedies Court Zone the applicant resides', value: 'Midlands'},
+      { tabItem: 'Please state in which Financial Remedies Court Zone the applicant resides', value: 'Midlands' },
       { tabItem: 'This should be the FRC local to the applicant', value: 'Nottingham FRC' },
       { tabItem: 'Where is the Applicant’s Local Court?', value: 'LEICESTER COUNTY COURT AND FAMILY COURT' },
       'SOLICITOR DETAILS',
@@ -55,13 +55,7 @@ export const updateRespondentNonRepresentedContactDetailsSolChangeTabData = [
       'RESPONDENT DETAILS',
       { tabItem: 'Current First and Middle names', value: 'Smeagol' },
       { tabItem: 'Current Last Name', value: 'Gollum' },
-      { tabItem: 'Is the Respondent currently a resident in a refuge?', value: 'Yes' },
-      { tabItem: 'Is the respondent represented ?', value: 'No' },
-      'RESPONDENT SERVICE ADDRESS DETAILS',
-      { tabItem: 'Building and Street', value: '10 Selsdon Road' },
-      { tabItem: 'Town or City', value: 'London' },
-      { tabItem: 'Country', value: 'United Kingdom' },
-      { tabItem: 'Postcode/Zipcode', value: 'NW2 7NE' }
+      { tabItem: 'Is the respondent represented ?', value: 'Yes' },
     ]
   }
 ];
