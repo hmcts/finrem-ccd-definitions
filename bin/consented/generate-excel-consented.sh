bba4d94f6985c27c54579d2044d97f0e19fdf2b4
erclaudio <claudio.andrei@icloud.com>
2026-04-29 16:09:07 +0100

    modified strategy

--- bin/consented/generate-excel-consented.sh
+++ bin/consented/generate-excel-consented.sh
@@ -1,2 +1,19 @@
 #!/usr/bin/env bash
-pushd ccd-definition-processor && CCD_DEF_CASE_TYPE_ID=FinancialRemedyMVP2 yarn --cwd ccd-definition-processor json2xlsx -D ../definitions/consented/json -e ${EXCLUDE} -o ../definitions/consented/xlsx/ccd-config-${FR_ENV:-base}-consented-${GIT_COMMIT:-base}.xlsx && popd
+WA_ENABLED=${WA_ENABLED:-false}
+
+if [ "$WA_ENABLED" = "true" ]; then
+  waExclusion="*-nonWA*"
+else
+  waExclusion="*-WA-*"
+fi
+
+fullExclusion="${EXCLUDE},${waExclusion}"
+
+pushd ccd-definition-processor && \
+  CCD_DEF_CASE_TYPE_ID=FinancialRemedyMVP2 \
+  CCD_DEF_PUBLISH=${CCD_DEF_PUBLISH:-N} \
+  yarn --cwd ccd-definition-processor json2xlsx \
+  -D ../definitions/consented/json \
+  -e ${fullExclusion} \
+  -o ../definitions/consented/xlsx/ccd-config-${FR_ENV:-base}-consented-${GIT_COMMIT:-base}.xlsx && \
+popd
--- bin/consented/generate-excel-preview-consented.sh
+++ bin/consented/generate-excel-preview-consented.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
-source "${ROOT_DIR}/default.env"
+#ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
+#source "${ROOT_DIR}/default.env"
+#
+#if [ "${1:-}" = "no-wa" ]; then
+#  FR_ENV=preview CCD_DEF_COS_URL=$COS_API_URL CCD_DEF_CCD_URL=$CCD_DATA_API_URL CCD_DEF_AAC_URL=$AAC_API_URL EXCLUDE='*-prod.json' yarn run generate-excel-consented
+#else
+#  FR_ENV=preview CCD_DEF_COS_URL=$COS_API_URL CCD_DEF_CCD_URL=$CCD_DATA_API_URL CCD_DEF_AAC_URL=$AAC_API_URL EXCLUDE='*-prod.json' yarn run generate-excel-wa-consented
+#fi
 
-if [ "${1:-}" = "no-wa" ]; then
-  FR_ENV=preview CCD_DEF_COS_URL=$COS_API_URL CCD_DEF_CCD_URL=$CCD_DATA_API_URL CCD_DEF_AAC_URL=$AAC_API_URL EXCLUDE='*-prod.json' yarn run generate-excel-consented
-else
-  FR_ENV=preview CCD_DEF_COS_URL=$COS_API_URL CCD_DEF_CCD_URL=$CCD_DATA_API_URL CCD_DEF_AAC_URL=$AAC_API_URL EXCLUDE='*-prod.json' yarn run generate-excel-wa-consented
-fi
+#!usr/bin/env
+WA_ENABLED=true CCD_DEF_PUBLISH=Y FR_ENV=aat ... EXCLUDE='*-prod.json' yarn run generate-excel-consented
