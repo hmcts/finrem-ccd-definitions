95653a905c33e2ff73544a050767bb9492922dd0
erclaudio <claudio.andrei@icloud.com>
2026-05-11 10:09:22 +0100

    addressed pr commnets

--- bin/consented/generate-excel-consented.sh
+++ bin/consented/generate-excel-consented.sh
@@ -3,17 +3,20 @@ WA_ENABLED=${WA_ENABLED:-false}
 if [ "$WA_ENABLED" = "true" ]; then
   waExclusion=""
 else
-    waExclusion="*-wa.json"
+  waExclusion="*-wa-nonprod.json"
 fi
 
-if [ -z "$waExclusion" ]; then
+if [ -z "$waExclusion" ] && [ -z "$EXCLUDE" ]; then
+  fullExclusion=""
+elif [ -z "$EXCLUDE" ]; then
+  fullExclusion="${waExclusion}"
+elif [ -z "$waExclusion" ]; then
   fullExclusion="${EXCLUDE}"
 else
   fullExclusion="${EXCLUDE},${waExclusion}"
 fi
 pushd ccd-definition-processor && \
   CCD_DEF_CASE_TYPE_ID=FinancialRemedyMVP2 \
-  CCD_DEF_PUBLISH=${CCD_DEF_PUBLISH:-N} \
   yarn --cwd ccd-definition-processor json2xlsx \
   -D ../definitions/consented/json \
   -e ${fullExclusion} \
