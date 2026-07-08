a5d6fea8c4553481c8478c957240d6a2057a2c77
Austen Stevens <austen.stevens@justice.gov.uk>
2026-07-08 09:58:37 +0100

    e2e payment test fixes

---
+++ playwright-e2e/test/consented/test_data/payment_fees.ts
@@ -0,0 +1,11 @@
+// Common fee data - update here if the fee changes
+export const APPLICATION_FEE = {
+    amount: '£62.00',
+    code: 'FEE0228',
+    type: 'Application (without notice)'
+};
+
+export const ORDER_SUMMARY: string[][] = [
+    [APPLICATION_FEE.code, APPLICATION_FEE.type, APPLICATION_FEE.amount],
+    ['', 'Total', APPLICATION_FEE.amount]
+];
--- playwright-e2e/test/contested/FR_applicationPaymentSubmission/application_payment_submission.spec.ts
+++ playwright-e2e/test/contested/FR_applicationPaymentSubmission/application_payment_submission.spec.ts
@@ -3,173 +3,243 @@ import config from '../../../config/config.ts';
 import { ContestedEvents } from '../../../config/case-data.ts';
 import { ContestedCaseFactory } from '../../../data-utils/factory/contested/ContestedCaseFactory.ts';
 import { YesNoRadioEnum } from '../../../pages/helpers/enums/RadioEnums.ts';
-import { applicationCaseSubmission, applicationCaseSubmissionHWF } from '../../../pages/helpers/PaymentSubmissionHelper.ts';
+import {
+    applicationCaseSubmission,
+    applicationCaseSubmissionHWF
+} from '../../../pages/helpers/PaymentSubmissionHelper.ts';
 import { envTestData } from '../../../data-utils/test_data/EnvTestDataConfig.ts';
+import { PAYMENT_FEES } from '../test_data/payment_fees.ts';
+import { ManageCaseDashboardPage } from '../../../pages/ManageCaseDashboardPage.ts';
+import { SigninPage } from '../../../pages/SigninPage.ts';
+
+const REFERENCE = 'Reference';
+
+async function loginAndOpenCase(
+    loginPage: SigninPage,
+    manageCaseDashboardPage: ManageCaseDashboardPage,
+    caseId: string
+) {
+    await manageCaseDashboardPage.visit();
+
+    await loginPage.loginWaitForPath(
+        config.applicant_solicitor.email,
+        config.applicant_solicitor.password,
+        config.manageCaseBaseURL,
+        config.loginPaths.cases
+    );
+
+    await manageCaseDashboardPage.navigateToCase(caseId);
+}
+
+
+function createPaymentData(fee: { amount: string; code: string; type: string; }) {
+    return {
+        caseEvent: ContestedEvents.applicationPaymentSubmission,
+        reference: REFERENCE,
+        amount: fee.amount,
+        feeCode: fee.code,
+        feeType: fee.type
+    };
+}
+
 
 test(
-  'Contested - Case Submission - PBA Payment Matrimonial',
-  { tag: ['@payment'] },
-  async ({
-    loginPage,
-    manageCaseDashboardPage,
-    caseDetailsPage,
-    solicitorAuthPage,
-    helpWithFeesPage,
-    paymentPage,
-    orderSummaryPage,
-    caseSubmissionPage,
-    checkYourAnswersPage
-  }) => {
-    // Create form A case
-    const caseId = await ContestedCaseFactory.createBaseContestedFormA();
+    'Contested - Case Submission - PBA Payment Matrimonial',
+    { tag: ['@payment'] },
+    async ({
+               loginPage,
+               manageCaseDashboardPage,
+               caseDetailsPage,
+               solicitorAuthPage,
+               helpWithFeesPage,
+               paymentPage,
+               orderSummaryPage,
+               caseSubmissionPage,
+               checkYourAnswersPage
+           }) => {
 
-    // Define common test data
-    const pbaNumber = envTestData.PBA_NUMBER;
-    const reference = 'Reference';
-    const hasHelpWithFees = YesNoRadioEnum.NO;
-    const amount = '£313.00';
-    const feeCode = 'FEE0229';
-    const feeType = 'Application for a financial order';
+        const caseId = await test.step('Create matrimonial Form A case', async () => {
+            return await ContestedCaseFactory.createBaseContestedFormA();
+        });
 
-    // Login as solicitor
-    await manageCaseDashboardPage.visit();
-    await loginPage.loginWaitForPath(
-      config.applicant_solicitor.email,
-      config.applicant_solicitor.password,
-      config.manageCaseBaseURL,
-      config.loginPaths.cases
-    );
-    await manageCaseDashboardPage.navigateToCase(caseId);
 
-    await applicationCaseSubmission(
-      caseDetailsPage,
-      solicitorAuthPage,
-      helpWithFeesPage,
-      paymentPage,
-      orderSummaryPage,
-      caseSubmissionPage,
-      checkYourAnswersPage,
-      {
-        caseEvent: ContestedEvents.applicationPaymentSubmission,
-        hasHelpWithFees: hasHelpWithFees,
-        pbaNumber: pbaNumber,
-        reference: reference,
-        amount: amount,
-        feeCode: feeCode,
-        feeType: feeType
-      }
-    );
-  }
+        await test.step('Login and open case', async () => {
+            await loginAndOpenCase(
+                loginPage,
+                manageCaseDashboardPage,
+                caseId
+            );
+        });
+
+
+        await test.step('Submit matrimonial PBA payment', async () => {
+
+            await applicationCaseSubmission(
+                caseDetailsPage,
+                solicitorAuthPage,
+                helpWithFeesPage,
+                paymentPage,
+                orderSummaryPage,
+                caseSubmissionPage,
+                checkYourAnswersPage,
+                {
+                    ...createPaymentData(PAYMENT_FEES.matrimonial),
+                    hasHelpWithFees: YesNoRadioEnum.NO,
+                    pbaNumber: envTestData.PBA_NUMBER
+                },
+                [
+                    [
+                        PAYMENT_FEES.matrimonial.code,
+                        PAYMENT_FEES.matrimonial.type,
+                        PAYMENT_FEES.matrimonial.amount
+                    ],
+                    [
+                        '',
+                        'Total',
+                        PAYMENT_FEES.matrimonial.amount
+                    ]
+                ]
+            );
+
+        });
+
+    }
 );
 
+
+
 test(
-  'Contested - Case Submission - PBA Payment Schedule 1 case',
-  { tag: ['@payment'] },
-  async ({
-    loginPage,
-    manageCaseDashboardPage,
-    caseDetailsPage,
-    solicitorAuthPage,
-    helpWithFeesPage,
-    paymentPage,
-    orderSummaryPage,
-    caseSubmissionPage,
-    checkYourAnswersPage
-  }) => {
-    // Create Schedule 1 case
-    const caseId = await ContestedCaseFactory.createSchedule1Case();
+    'Contested - Case Submission - PBA Payment Schedule 1 case',
+    { tag: ['@payment'] },
+    async ({
+               loginPage,
+               manageCaseDashboardPage,
+               caseDetailsPage,
+               solicitorAuthPage,
+               helpWithFeesPage,
+               paymentPage,
+               orderSummaryPage,
+               caseSubmissionPage,
+               checkYourAnswersPage
+           }) => {
 
-    // Define common test data
-    const pbaNumber = envTestData.PBA_NUMBER;
-    const reference = 'Reference';
-    const hasHelpWithFees = YesNoRadioEnum.NO;
-    const amount = '£263.00';
-    const feeCode = 'FEE0318';
-    const feeType = 'Financial provision for children (paragraph 1(1) or (4), 2(1) or (5), 5(6), 6(5), (7) or (8), 8(2), 10(2), 11 or 14(1) of Schedule 1) ';
 
-    // Login as solicitor
-    await manageCaseDashboardPage.visit();
-    await loginPage.loginWaitForPath(
-      config.applicant_solicitor.email,
-      config.applicant_solicitor.password,
-      config.manageCaseBaseURL,
-      config.loginPaths.cases
-    );
-    await manageCaseDashboardPage.navigateToCase(caseId);
+        const caseId = await test.step('Create Schedule 1 case', async () => {
+            return await ContestedCaseFactory.createSchedule1Case();
+        });
 
-    await applicationCaseSubmission(
-      caseDetailsPage,
-      solicitorAuthPage,
-      helpWithFeesPage,
-      paymentPage,
-      orderSummaryPage,
-      caseSubmissionPage,
-      checkYourAnswersPage,
-      {
-        caseEvent: ContestedEvents.applicationPaymentSubmission,
-        hasHelpWithFees: hasHelpWithFees,
-        pbaNumber: pbaNumber,
-        reference: reference,
-        amount: amount,
-        feeCode: feeCode,
-        feeType: feeType
-      },
-      [
-        ['FEE0318', 'Financial provision for children (paragraph 1(1) or (4), 2(1) or (5), 5(6), 6(5), (7) or (8), 8(2), 10(2), 11 or 14(1) of Schedule 1)', '£263.00'],
-        ['', 'Total', '£263.00']
-      ]
-    );
-  }
+
+        await test.step('Login and open case', async () => {
+
+            await loginAndOpenCase(
+                loginPage,
+                manageCaseDashboardPage,
+                caseId
+            );
+
+        });
+
+
+        await test.step('Submit Schedule 1 PBA payment', async () => {
+
+
+            await applicationCaseSubmission(
+                caseDetailsPage,
+                solicitorAuthPage,
+                helpWithFeesPage,
+                paymentPage,
+                orderSummaryPage,
+                caseSubmissionPage,
+                checkYourAnswersPage,
+                {
+                    ...createPaymentData(PAYMENT_FEES.schedule1),
+                    hasHelpWithFees: YesNoRadioEnum.NO,
+                    pbaNumber: envTestData.PBA_NUMBER
+                },
+                [
+                    [
+                        PAYMENT_FEES.schedule1.code,
+                        PAYMENT_FEES.schedule1.type,
+                        PAYMENT_FEES.schedule1.amount
+                    ],
+                    [
+                        '',
+                        'Total',
+                        PAYMENT_FEES.schedule1.amount
+                    ]
+                ]
+            );
+
+
+        });
+
+    }
 );
 
+
+
 test(
-  'Contested - Case Submission - HWF Payment',
-  { tag: ['@payment', '@hwf'] },
-  async ({
-    loginPage,
-    manageCaseDashboardPage,
-    caseDetailsPage,
-    solicitorAuthPage,
-    helpWithFeesPage,
-    orderSummaryPage,
-    caseSubmissionPage,
-    checkYourAnswersPage
-  }) => {
-    // Create case
-    const caseId = await ContestedCaseFactory.createBaseContestedFormA();
+    'Contested - Case Submission - HWF Payment',
+    { tag: ['@payment', '@hwf'] },
+    async ({
+               loginPage,
+               manageCaseDashboardPage,
+               caseDetailsPage,
+               solicitorAuthPage,
+               helpWithFeesPage,
+               orderSummaryPage,
+               caseSubmissionPage,
+               checkYourAnswersPage
+           }) => {
 
-    // Define common test data
-    const reference = 'Reference';
-    const hasHelpWithFees = YesNoRadioEnum.YES;
-    const hwfCode = 'HWF123123';
-    const amount = '£313.00';
-    const feeCode = 'FEE0229';
-    const feeType = 'Application for a financial order';
 
-    // Login as caseworker
-    await manageCaseDashboardPage.visit();
-    await loginPage.loginWaitForPath(config.applicant_solicitor.email, config.applicant_solicitor.password, config.manageCaseBaseURL, config.loginPaths.cases);
-    await manageCaseDashboardPage.navigateToCase(caseId);
-    await applicationCaseSubmissionHWF(
-      caseDetailsPage,
-      solicitorAuthPage,
-      helpWithFeesPage,
-      orderSummaryPage,
-      caseSubmissionPage,
-      checkYourAnswersPage,
-      {
-        caseEvent: ContestedEvents.applicationPaymentSubmission,
-        hasHelpWithFees: hasHelpWithFees,
-        hwfCode: hwfCode,
-        reference: reference,
-        amount: amount,
-        feeCode: feeCode,
-        feeType: feeType
-      },
-      [
-        ['FEE0229', 'Application for a financial order', '£313.00'],
-        ['', 'Total', '£313.00']
-      ]
-    );
-  }
+        const caseId = await test.step('Create matrimonial Form A case', async () => {
+            return await ContestedCaseFactory.createBaseContestedFormA();
+        });
+
+
+        await test.step('Login and open case', async () => {
+
+            await loginAndOpenCase(
+                loginPage,
+                manageCaseDashboardPage,
+                caseId
+            );
+
+        });
+
+
+        await test.step('Submit HWF payment', async () => {
+
+
+            await applicationCaseSubmissionHWF(
+                caseDetailsPage,
+                solicitorAuthPage,
+                helpWithFeesPage,
+                orderSummaryPage,
+                caseSubmissionPage,
+                checkYourAnswersPage,
+                {
+                    ...createPaymentData(PAYMENT_FEES.matrimonial),
+                    hasHelpWithFees: YesNoRadioEnum.YES,
+                    hwfCode: 'HWF123123'
+                },
+                [
+                    [
+                        PAYMENT_FEES.matrimonial.code,
+                        PAYMENT_FEES.matrimonial.type,
+                        PAYMENT_FEES.matrimonial.amount
+                    ],
+                    [
+                        '',
+                        'Total',
+                        PAYMENT_FEES.matrimonial.amount
+                    ]
+                ]
+            );
+
+
+        });
+
+    }
 );
