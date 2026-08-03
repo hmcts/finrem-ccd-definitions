519055adaadfb9bcc623caef52ce63e8d36fd42d
Ashley Wong <im@so99y.com>
2026-08-03 17:38:23 +0100

    Remove unused codes.

--- playwright-e2e/pages/events/manage-express-case/ManageExpressCasePage.ts
+++ playwright-e2e/pages/events/manage-express-case/ManageExpressCasePage.ts
@@ -5,9 +5,7 @@ export class ManageExpressCasePage extends BaseJourneyPage {
   private readonly manageExpressCaseTitle: Locator;
 
   private readonly expressPilotQuestionHeader: Locator;
-  private readonly expressPilotQuestion: Locator;
 
-  private readonly expressPilotWasWithdrawnLabel: Locator;
   private readonly expressPilotNotEnrolledLabel: Locator;
 
   private readonly removeFromExpressPilotWarning: Locator;
@@ -18,11 +16,9 @@ export class ManageExpressCasePage extends BaseJourneyPage {
     this.manageExpressCaseTitle = page.getByRole('heading', { name: 'Manage Express Case' });
 
     this.expressPilotQuestionHeader = page.getByText('Express Pilot?');
-    this.expressPilotQuestion = page.getByLabel('Express Pilot?');
     this.removeFromExpressPilotWarning = page.getByText('Warning: Once you remove a case from the Express Financial Remedy Pilot, it cannot be re-added so this will be a permanent change.');
     this.confirmRemoveCaseFromExpressPilot = page.locator('input[type="checkbox"][name="confirmRemoveCaseFromExpressPilot"]');
 
-    this.expressPilotWasWithdrawnLabel = page.getByText('This case was removed from the Express Financial Remedy Pilot.');
     this.expressPilotNotEnrolledLabel = page.getByText('This case is not enrolled in the Express Financial Remedy Pilot.');
   }
 
@@ -42,17 +38,9 @@ export class ManageExpressCasePage extends BaseJourneyPage {
     await this.selectExpressPilotQuestion('No');
   }
 
-  async selectExpressPilotQuestionYes() {
-    await this.selectExpressPilotQuestion('Yes');
-  }
-
   async verifyRemoveFromExpressPilotElements() {
     expect(this.removeFromExpressPilotWarning).toBeVisible();
     expect(this.confirmRemoveCaseFromExpressPilot).toBeVisible();
-  }
-
-  async verifyExpressPilotWasWithdrawn() {
-    expect(this.expressPilotWasWithdrawnLabel).toBeVisible();
   }
 
   async verifyExpressPilotNotEnrolled() {
