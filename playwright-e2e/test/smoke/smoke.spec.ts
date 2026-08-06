ddb0b16f6158b71eaeb7a0aa480c9d106dd0ae26
Sahil Goonraz <sahil.goonraz@solirius.com>
2026-08-06 10:24:50 +0100

    Debug: Add caseworker email and password to logs

--- playwright-e2e/test/smoke/smoke.spec.ts
+++ playwright-e2e/test/smoke/smoke.spec.ts
@@ -10,6 +10,9 @@ test(
       manageCaseDashboardPage
     }
   ) => {
+    console.log(`Caseworker Email: ${config.caseWorker.email}`);
+    console.log(`Caseworker Password: ${config.caseWorker.password}`);
+
     await manageCaseDashboardPage.visit();
     await loginPage.loginWaitForPath(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL, config.loginPaths.worklist);
   }
