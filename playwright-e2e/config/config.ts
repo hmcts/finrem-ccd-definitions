775d2c653bc613de394c4ee664b9845e3ba3c7b4
lawrence.reynolds <278118191+LawrenceReynolds-HMCTS@users.noreply.github.com>
2026-08-12 12:47:23 +0100

    Removing empty line.

--- playwright-e2e/config/config.ts
+++ playwright-e2e/config/config.ts
@@ -8,7 +8,6 @@ const env = process.env.RUNNING_ENV || 'aat';
 // Any new data added below will immediately be available wherever config is imported.
 const configuration = {
 
-
   waEnabled: process.env.ENABLE_WA || false,
 
   // URLs
