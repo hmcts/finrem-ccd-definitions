54544b0a162ba67dc1932bc070ffff2384732f33
Sahil Goonraz <sahil.goonraz@solirius.com>
2026-08-28 15:09:20 +0100

    Test

--- playwright-e2e/test/smoke/smoke.spec.ts
+++ playwright-e2e/test/smoke/smoke.spec.ts
@@ -2,15 +2,18 @@ import { test } from '../../fixtures/fixtures';
 import config from '../../config/config';
 
 test('Smoke Test - Check env running and can login',
-  { tag: ['@smoke-test'] },
-  async (
-    {
-      loginPage,
-      manageCaseDashboardPage
+    { tag: ['@smoke-test'] },
+    async (
+        {
+            loginPage,
+            manageCaseDashboardPage
+        }
+    ) => {
+        await manageCaseDashboardPage.visit();
+        if (config.waEnabled) {
+            await loginPage.loginWaitForPath(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL, config.loginPaths.worklist);
+        } else {
+            await loginPage.loginWaitForPath(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL, config.loginPaths.cases);
+        }
     }
-  ) => {
-    await manageCaseDashboardPage.visit();
-    await loginPage.loginWaitForPath(config.caseWorker.email, config.caseWorker.password, config.manageCaseBaseURL, config.loginPaths.cases);
-    
-  }
 );
