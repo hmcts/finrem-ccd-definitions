96dc0808595c747503761d7ee0bc7c3f9418e1ff
Tom-HMCTS <167980396+Tom-HMCTS@users.noreply.github.com>
2026-09-18 12:03:01 +0100

    revert

--- playwright-e2e/test/consented/WA_Tasks/FR_check&IssueApplication/check_issue_application.spec.ts
+++ playwright-e2e/test/consented/WA_Tasks/FR_check&IssueApplication/check_issue_application.spec.ts
@@ -64,7 +64,7 @@ test.describe('Check and Issue Application task tests', () => {
   for (const { user, completionAction } of checkIssueApplicationUserScenarios) {
     test(
       `${user.name} completes the task using ${completionAction}`,
-      { tag: ['@waTasks'] },
+      { tag: completionAction === ISSUE_APPLICATION ? ['@waTasks', '@preview'] : ['@waTasks'] },
       async ({
         loginPage,
         manageCaseDashboardPage,
@@ -136,6 +136,7 @@ test.describe('Check and Issue Application task tests', () => {
       manageCaseDashboardPage,
       taskUiChecks
     }) => {
+      test.setTimeout(15 * 60 * 1000);
 
       const sessionPages = {
         loginPage,
